862ede459bfcf402d62a2a83afc943158cd7c515
helen-zhu <helen.m.zhu@gmail.com>
2021-04-07 16:20:26 -0400

    testing

--- R/segment.and.fit.R
+++ R/segment.and.fit.R
@@ -45,21 +45,15 @@ segment.and.fit = function(
   GENEPEAKSGR = unlist(GenomicRanges::reduce(GENEPEAKSGR))
 
   # Peak Coverage
-  REDUCED.GENE.PEAKS.GR = GenomicRanges::reduce(GENEPEAKSGR)
+  GENEGR = GenomicRanges::GRanges(seqnames = GENEINFO$chr, IRanges::IRanges(start = 1, end = GENEINFO$exome_length), strand = GENEINFO$strand)
   PEAK.COVERAGE = GenomicRanges::coverage(GENEPEAKSGR)
-  BINS = unlist(GenomicRanges::tile(REDUCED.GENE.PEAKS.GR, width = 1))
+  BINS = unlist(GenomicRanges::tile(GENEGR, width = 1))
   BIN.COUNTS = data.frame(GenomicRanges::binnedAverage(BINS, PEAK.COVERAGE, "Coverage"), stringsAsFactors = F)
-  add.zero.counts = setdiff(1:GENEINFO$exome_length, BIN.COUNTS$start)
-  if(length(add.zero.counts) > 0){
-    add.zero.counts = data.frame("start" = add.zero.counts, "Coverage" = 0, stringsAsFactors = F)
-    BIN.COUNTS = rbind(BIN.COUNTS[,c("start", "Coverage")], add.zero.counts)
-  }
-  BIN.COUNTS = BIN.COUNTS[order(BIN.COUNTS$start),]
 
   # Segmenting & Determining which segments are peaks
   # Test 1: Fit smoothing spline before finding the peaks
   smooth.coverage = smooth.spline( BIN.COUNTS$start, BIN.COUNTS$Coverage, spar = 0.4)
-  p = find.peaks(x = -smooth.coverage$y, m = 150, diff.threshold = 10^-5)
+  p = find.peaks(x = -smooth.coverage$y, m = 150, diff.threshold = 10^-7)
   # Test 2: As-is
   # p = find.peaks(-BIN.COUNTS$Coverage, m = 150)
   # Test 3: Moving Average
@@ -104,11 +98,35 @@ segment.and.fit = function(
   results = data.frame()
   models = list()
   for(i in 1:nrow(seg.df)){
+    # cat(i, "\n")
     x = peak.counts[peak.counts >= seg.df$start[i] & peak.counts <= seg.df$end[i]]
-    simple.model.names <- c("norm", "gamma", "unif")
+    mod = list()
 
-    mod = lapply(simple.model.names, function(distr) fitdistrplus::fitdist(x, distr))
-    names(mod) <- simple.model.names
+    # Uniform Distribution (This is unlikely to fail, I know)
+    tryCatch(
+      expr = {
+        mod$unif <- fitdistrplus::fitdist(
+          data = x,
+          distr = "unif")
+      },
+      error = function(e) {
+        warning(sprintf("Error in fitdist unif for segment [%d, %d]", seg.df$start[i], seg.df$end[i]))
+        print(e)
+      }
+    )
+
+    # Gamma Distribution (Change to truncated gamma)
+    tryCatch(
+      expr = {
+        mod$gamma <- fitdistrplus::fitdist(
+          data = x,
+          distr = "gamma")
+      },
+      error = function(e) {
+        warning(sprintf("Error in fitdist gamma for segment [%d, %d]", seg.df$start[i], seg.df$end[i]))
+        print(e)
+      }
+    )
 
     # Attempt to fit a truncated normal distribution
     # Wrap in a tryCatch incase MLE fails to converge
