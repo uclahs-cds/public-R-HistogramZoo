6272222b9bad5b52a619f9e7210fa4a8468e31a5
Stefan Eng <stefaneng@mednet.ucla.edu>
2021-12-17 11:31:39 -0800

    Add repeated simulation

--- inst/simulate.peaks.R
+++ inst/simulate.peaks.R
@@ -11,3 +11,19 @@ plot.segments(x.norm.mix, s = new.s.bin)
 
 res[res$final == 1, ]
 res.trunc[res.trunc$final == 1, ]
+
+# Simulate one normal, one gamma, one unif
+total.iterations <- 2
+res <- lapply(seq(1, total.iterations), function(iter) {
+  set.seed(iter)
+  x.norm.mix = obs.to.int.hist(c(rnorm(80, mean = 10), rgamma(100, 2, 2) + 20, runif(50, min = 40, max = 50)))
+
+  res.non.trunc = segment.fit.agnostic(x.norm.mix)
+
+  res.trunc = segment.fit.agnostic(x.norm.mix, truncated.models = TRUE, remove.low.entropy =T)
+
+  cbind(
+    rbind(res.non.trunc[res.non.trunc$final == 1, ], res.trunc[res.trunc$final == 1, ]),
+    iter = iter
+  )
+})
