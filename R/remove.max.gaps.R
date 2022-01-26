c4203cdee4eecc90cc6c0742d535c807bbe6a81a
helen-zhu <helen.m.zhu@gmail.com>
2022-01-25 21:11:01 -0500

    temp fix

--- R/remove.max.gaps.R
+++ R/remove.max.gaps.R
@@ -21,3 +21,19 @@ remove.max.gaps.agnostic = function(p, max.gaps, remove.short.segment = 1) {
 
   new.p
 }
+
+remove.max.gaps.iranges = function(p, max.gaps, remove.short.segment = 1){
+  # Create IRanges object with the segments identified by p
+  l.p = length(p) - 1
+  p.ir = IRanges::IRanges(start = p[1:l.p], end = c(p[2:l.p] - 1, p[l.p+1]))
+  p.ir = S4Vectors::split(p.ir, seq_along(p.ir))
+  # Create IRanges object with max gap
+  maxgap.ir = if(nrow(max.gaps) > 0) IRanges::IRanges(start = max.gaps[,1], end = max.gaps[,2]) else IRanges::IRanges()
+  # Subtract maxgaps
+  segs.ir = IRanges::setdiff(p.ir, maxgap.ir)
+  segs.ir = unlist(segs.ir)
+  # Remove short segments
+  segs.ir = segs.ir[IRanges::width(segs.ir) > remove.short.segment]
+  # Create a list of ranges
+  lapply(seq_along(segs.ir), function(i) list(start = IRanges::start(segs.ir[i]), end = IRanges::end(segs.ir[i])))
+}
