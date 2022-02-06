4db2a27872b88276b40d174eb01aabc8010a8865
helen-zhu <helen.m.zhu@gmail.com>
2022-02-05 21:57:40 -0500

    changing the place where we filter out edge cases

--- R/segment.fit.agnostic.R
+++ R/segment.fit.agnostic.R
@@ -70,14 +70,15 @@ segment.fit.agnostic <- function(
     p.init = unname(c(segs['start'], chgpts[chgpts > segs['start'] & chgpts < segs['end']], segs['end']))
     p.init = sort(unique(p.init)) # meaningful gaps local also needs p.init to be sorted so temporarily adding this back
     p = ftc.helen(x, p.init, eps) # Maybe change this to FTC if Stefan has more error checks in place
-    p = p[(abs(p - segs['start']) > min.gap.size & abs(p - segs['end']) > min.peak.size) | p %in% segs]
 
     # Max Gap
     if(remove.low.entropy) {
       mgaps =  meaningful.gaps.local(x = x, seg.points = p, change.points = p.init, min.gap = min.gap.size)
+      p = p[(abs(p - segs['start']) > min.gap.size & abs(p - segs['end']) > min.peak.size) | p %in% segs]
       p.pairs = remove.max.gaps.agnostic(p = p, max.gaps = mgaps, remove.short.segment = min.peak.size) # remove.short.segment can also be used to filter min.peak.size, but doesn't extend to non remove low entropy cases
       # p.pairs$max.gap.removed = TRUE
     } else {
+      p = p[(abs(p - segs['start']) > min.gap.size & abs(p - segs['end']) > min.peak.size) | p %in% segs]
       p.pairs = index.to.start.end.shifted1bp(p)
       # p.pairs$max.gap.removed = FALSE
     }
