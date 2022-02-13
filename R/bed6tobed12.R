ddfcd6a061dd6c56936913d854c80cebd973b479
helen-zhu <helen.m.zhu@gmail.com>
2022-02-12 20:16:57 -0500

    fixing summarize.results

--- R/bed6tobed12.R
+++ R/bed6tobed12.R
@@ -1,4 +1,3 @@
-# TODO: Check that this is compatible with Base 0 and Base 1 formatting
 #' Takes a GRanges object and creates a BED12 GRanges object
 #'
 #' @param gr TODO
@@ -9,14 +8,14 @@ bed6tobed12 = function(
   gr
 ){
   bed12.gr = range(gr)
-  # blockCount
+  S4Vectors::mcols(bed12.gr)$score = 0
+  S4Vectors::mcols(bed12.gr)$thickStart = GenomicRanges::start(bed12.gr)
+  S4Vectors::mcols(bed12.gr)$thickEnd = GenomicRanges::end(bed12.gr)
+  S4Vectors::mcols(bed12.gr)$itemRgb = 0
   S4Vectors::mcols(bed12.gr)$blockCount = length(gr)
-  # blockSizes
   blockSizes = IRanges::width(gr) - 1
   S4Vectors::mcols(bed12.gr)$blockSizes = paste0(blockSizes, ",", collapse = "")
-  # blockStarts
   blockStarts = GenomicRanges::start(gr) - GenomicRanges::start(bed12.gr)
   S4Vectors::mcols(bed12.gr)$blockStarts = paste0(blockStarts, ",", collapse = "")
-  # Return GRanges object
   bed12.gr
 }
--- R/summarize.results.R
+++ R/summarize.results.R
@@ -1,44 +1,70 @@
+
+extract.stats = function(models, gene, peak.id){
+  mod = models$majority.vote
+  data.frame(
+    "gene" = gene,
+    "peak.id" = peak.id,
+    "start" = mod$seg.start,
+    "end" = mod$seg.end,
+    "value" = mod$value,
+    "metric" = mod$metric,
+    "dist" = mod$dist,
+    "params" = dput.str(mod$par)
+  )
+}
+
 # TODO: Create a reasonable format for basic histograms
 #' Formats results
 #'
-#' @param coverage.model.obj TODO
+#' @param results.object TODO
 #' @param output.format TODO
 #'
 #' @return
 #' @export
 summarize.results = function(
-  coverage.model.obj,
-  output.format = c("stats.only", "BED6", "BED12")
+  results.object,
+  output.format = c("stats.only", "bed")
 ){
 
   output.format = match.arg(output.format)
-
-  # Coverage.model.obj
-  gene.model = coverage.model.obj$gene.model
-  histogram.ids = names(gene.model)
-  histogram.bin.size = coverage.model.obj$histogram.bin.size
-  results = lapply(coverage.model.obj$results, `[[`, "models")
-
-  formatted.results = GenomicRanges::GRanges()
-  for(i in histogram.ids){
-    x = gene.model[i]
-    x = unlist(x)
-    bins = GenomicRanges::tile(x = x, width = histogram.bin.size)
-    bins = unlist(bins)
-    res = results[[i]]
-    for(j in seq_along(res)){
-      id =  paste0(i, ":", j)
-      final.mod = res[[j]]$majority.vote
-      f.res = GenomicRanges::reduce(bins[final.mod$seg.start:final.mod$seg.end])
-      f.res = base1.to.base0(f.res)
-      f.res = if(output.format == "BED12") bed6tobed12(f.res)
-      S4Vectors::mcols(f.res)$name = id
-      S4Vectors::mcols(f.res)[,c( "value", "metric", "dist")] = final.mod[c( "value", "metric", "dist")]
-      S4Vectors::mcols(f.res)$params = dput.str(final.mod$par)
-      formatted.results[id] = f.res
-    }
+  if(is.null(results.object$gene.model) & output.format == "bed"){
+    stop("A gene model is required for BED output.")
   }
-  # Reordering the columns
-  S4Vectors::mcols(formatted.results) = S4Vectors::mcols(formatted.results)[,c("name", "value", "metric", "dist", "params", "blockCount", "blockSizes", "blockStarts")]
-  data.frame(formatted.results)
+
+  # Recovering output stats
+  results = results.object[['results']]
+  results = lapply(results, `[[`, "models")
+  results.tbl = lapply(seq_along(results), function(gene){
+    res.gene = results[[gene]]
+    lapply(seq_along(res.gene), function(i){
+      extract.stats(
+        models = res.gene[[i]],
+        gene = names(results)[[gene]],
+        peak.id = i)
+    })
+  })
+  results.tbl = do.call('rbind.data.frame', unlist(results.tbl, recursive = F))
+
+  # Recovering coordinates
+  if(output.format == "bed"){
+    gene.model = results.object$gene.model
+    histogram.bin.size = results.object$histogram.bin.size
+    bed.coords = apply(results.tbl, 1, function(peak){
+      gene = peak['gene']
+      x = unlist(gene.model[gene])
+      bins = GenomicRanges::tile(x = x, width = histogram.bin.size)
+      bins = unlist(bins)
+      coords.gr = GenomicRanges::reduce(bins[peak['start']:peak['end']])
+      coords.gr = base1.to.base0(coords.gr)
+      coords.gr = bed6tobed12(coords.gr)
+      coords.gr
+    })
+    coords.tbl = do.call(c, bed.coords)
+    coords.tbl = data.frame(coords.tbl)
+    # TODO: Extract the correct column names for BED12
+    # TODO: Remove the start and end for stats
+    results.tbl = cbind.data.frame(results.tbl, coords.tbl)
+  }
+
+  results.tbl
 }
