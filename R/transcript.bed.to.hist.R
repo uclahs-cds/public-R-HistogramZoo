59dff7b8eff30adb09c75b2b796ec06aa0d42743
helen-zhu <helen.m.zhu@gmail.com>
2022-02-13 18:24:33 -0500

    minor edits

--- R/transcript.bed.to.hist.R
+++ R/transcript.bed.to.hist.R
@@ -2,29 +2,29 @@
 #'
 #' @param filenames TODO
 #' @param n_fields TODO
-#' @param regions.of.interest TODO
 #' @param gtf.file TODO
 #' @param gene.or.transcript TODO
+#' @param select.ids TODO
 #' @param histogram.bin.size TODO
 #' @param ... TODO
 #'
 #' @return
 #' @export
-transcript.bed.to.hist = function(
+transcript.bed.to.histogram = function(
   filenames,
   n_fields = 3,
-  regions.of.interest = NULL,
   gtf.file = NULL,
   gene.or.transcript = c("gene", "transcript"),
+  select.ids = NULL,
   histogram.bin.size = 1,
   ...
 ){
 
   peaks = lapply(filenames, function(filename){
-    segs = valr::read_bed( filename, n_fields = n_fields, ...)
+    segs = valr::read_bed( filename, n_fields = n_fields, comment = "#")
     if(n_fields == 12){ segs = valr::bed12_to_exons( segs ) }
     segs.gr = GenomicRanges::makeGRangesFromDataFrame( segs, keep.extra.columns = T )
-    segs.gr = base0.to.base1(segs.gr)
+    segs.gr = ConsensusPeaks:::base0.to.base1(segs.gr)
     segs.gr = S4Vectors::split(segs.gr, f = segs.gr$name)
     segs.gr = GenomicRanges::reduce(segs.gr)
     unlist(segs.gr)
@@ -32,21 +32,20 @@ transcript.bed.to.hist = function(
   peaks = do.call(c, peaks)
   peaks = split(peaks, f = names(peaks))
 
-  genes = names(peaks)
-  if(!is.null(regions.of.interest)){ genes = intersect(genes, regions.of.interest)}
+  ids = names(peaks)
+  if(!is.null(select.ids)){
+    ids = intersect(ids, select.ids)
+  }
 
   regions = gtf.to.genemodel(
     gtf.file = gtf.file,
-    split.by.strand = F,
     gene.or.transcript = gene.or.transcript,
-    select.chrs = NULL,
-    select.genes = genes)
+    select.ids = ids,
+    ...)
 
-  # TODO: Error check to make sure names and genes match
-
-  histogram.coverage =  vector("list", length(genes))
-  names(histogram.coverage) = genes
-  for(i in genes){
+  histogram.coverage =  vector("list", length(ids))
+  names(histogram.coverage) = ids
+  for(i in ids){
     peaks.cov = GenomicRanges::coverage(peaks[[i]])
     bins = GenomicRanges::tile(x = regions[[i]], width = histogram.bin.size)
     bins = unlist(bins)
@@ -55,9 +54,7 @@ transcript.bed.to.hist = function(
       bins = bins,
       numvar = peaks.cov,
       varname = "cvg")
-    cvg = cvg$cvg
-    names(cvg) = NULL
-    histogram.coverage[[i]] <- cvg
+    histogram.coverage[[i]] <- cvg$cvg
   }
 
   list(
