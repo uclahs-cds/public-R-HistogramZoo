6c54d317c0367900c82638033e8b755ab63ca80e
helen-zhu <helen.m.zhu@gmail.com>
2022-02-17 23:15:27 -0500

    updating GRanges utils tests

--- tests/testthat/test-GRanges_utils.R
+++ tests/testthat/test-GRanges_utils.R
@@ -1,6 +1,6 @@
 context("Testing input and parameter variations to GRanges utils")
 
-granges = GenomicRanges(
+granges = GenomicRanges::GRanges(
   seqnames = "chr1",
   IRanges::IRanges(
     start = c(3, 5),
@@ -9,26 +9,26 @@ granges = GenomicRanges(
   strand = "+"
 )
 
-test_that("base0tobase1 works", {
-  
-  base1 = base0tobase1(granges)
-  expect_equal(GenomicRanges::start(granges), c(4, 6))
-  expect_equal(GenomicRanges::end(granges), c(4, 9))
-  
+test_that("base0.to.base1 works", {
+
+  base1 = base0.to.base1(granges)
+  expect_equal(GenomicRanges::start(base1), c(4, 6))
+  expect_equal(GenomicRanges::end(base1), c(4, 9))
+
 })
 
-test_that("base1tobase0 works", {
-  
-  base0 = base1tobase0(granges)
-  expect_equal(GenomicRanges::start(granges), c(2, 4))
-  expect_equal(GenomicRanges::end(granges), c(4, 9))
-  
+test_that("base1.to.base0 works", {
+
+  base0 = base1.to.base0(granges)
+  expect_equal(GenomicRanges::start(base0), c(2, 4))
+  expect_equal(GenomicRanges::end(base0), c(4, 9))
+
 })
 
 test_that("generate.identifiers works", {
-  
+
   id = generate.identifiers(granges)
   expect_length(id, 2)
   expect_equal(id, c("chr1:3-4", "chr1:5-9"))
-  
+
 })
