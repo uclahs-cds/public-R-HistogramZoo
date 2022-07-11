14e99a6f2a6bc89b4fc41523431ed7e86ae4c3c2
helen-zhu <helen.m.zhu@gmail.com>
2022-07-11 00:38:34 -0400

    feat: two methods for SummarizeResults, one for GenomicHistogram and one for Histogram

--- R/summarize.results.R
+++ R/summarize.results.R
@@ -14,16 +14,28 @@ extract.stats = function(models, gene = NULL, peak.id){
   df
 }
 
+# Method Dispatch
+SummarizeResults = function(x, ...){
+  UseMethod('SummarizeResults', x, ...)
+}
+
 #' Formats results of segment.fit.agnostic
 #'
 #' @param result TODO
 #'
 #' @return TODO
 #' @export
-summarize.results.agnostic = function(
+SummarizeResults.Histogram = function(
   result = NULL
 ){
-  res = result[['models']]
+
+  # Error checking
+  stopifnot(inherits(result, "Histogram"))
+  if(is.null(attr(result, "models")) | is.null(attr(result, "p"))){
+    stop("No computed results. Please run SegmentAndFit first.")
+  }
+
+  res = attr(result, "models")
   results.tbl = lapply(seq_along(res), function(i){
     extract.stats(
       models = res[[i]],
@@ -42,42 +54,35 @@ summarize.results.agnostic = function(
 #'
 #' @return TODO
 #' @export
-summarize.results.bulk = function(
+SummarizeResults.GenomicHistogram = function(
   result = NULL,
   output.format = c("stats.only", "bed")
 ){
 
-  # Output format
+  # Error checking
+  stopifnot(inherits(result, "GenomicHistogram"))
+  if(is.null(attr(result, "models")) | is.null(attr(result, "p"))){
+    stop("No computed results. Please run SegmentAndFit first.")
+  }
   output.format = match.arg(output.format)
 
   # Recovering output stats
-  res = result[['results']]
-  res = lapply(res, `[[`, "models")
-  results.tbl = lapply(seq_along(res), function(gene){
-    res.gene = res[[gene]]
-    lapply(seq_along(res.gene), function(i){
-      extract.stats(
-        models = res.gene[[i]],
-        gene = names(res)[[gene]],
-        peak.id = i)
-    })
+  res = attr(result, "models")
+  results.tbl = lapply(seq_along(res), function(i){
+    extract.stats(
+      models = res[[i]],
+      gene = NULL,
+      peak.id = i)
   })
-  results.tbl = do.call('rbind.data.frame', unlist(results.tbl, recursive = F))
-
-  if(is.null(result$gene.model) & output.format == "bed"){
-    stop("A gene model is required for BED output.")
-  }
+  results.tbl = do.call('rbind.data.frame', results.tbl)
 
   # Recovering coordinates for genes
   if(output.format == "bed"){
-    gene.model = result$gene.model
-    histogram.bin.size = result$histogram.bin.size
+    interval_start = attr(result, "interval_start")
+    interval_end = attr(result, "interval_end")
+    bins = IRanges::IRanges(start = interval_start, end = interval_end)
     bed.coords = apply(results.tbl, 1, function(peak){
-      gene = peak['gene']
-      x = unlist(gene.model[gene])
-      bins = GenomicRanges::tile(x = x, width = histogram.bin.size)
-      bins = unlist(bins)
-      coords.gr = GenomicRanges::reduce(bins[peak['start']:peak['end']])
+      coords.gr = IRanges::reduce(bins[peak['start']:peak['end']])
       coords.gr = base1.to.base0(coords.gr)
       coords.gr = bed6tobed12(coords.gr)
       coords.gr
@@ -85,7 +90,12 @@ summarize.results.bulk = function(
     coords.tbl = do.call(c, bed.coords)
     coords.tbl = data.frame(coords.tbl)
     coords.tbl = subset(coords.tbl, select=-width)
-    colnames(coords.tbl)[colnames(coords.tbl) == "seqnames"] <- "chr"
+
+    # Technically this is the only part that might be unique to GenomicHistogram
+    coords.tbl['chr'] <- attr(results, "chr")
+    coords.tbl['strand'] <- attr(results, "strand")
+
+    # TODO: Potentially rewrite the bottom so that this is more efficient
     results.tbl = subset(results.tbl, select=-c(start, end))
     results.tbl = cbind.data.frame(results.tbl, coords.tbl)
   }
