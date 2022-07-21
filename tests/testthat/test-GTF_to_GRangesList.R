8103c1efe309aaf192d67110237579cc094b4e63
helen-zhu <helen.m.zhu@gmail.com>
2022-07-21 00:14:23 -0400

    test: updating GTF_to_GRangesList test parameters of function

--- tests/testthat/test-GTF_to_GRangesList.R
+++ tests/testthat/test-GTF_to_GRangesList.R
@@ -1,20 +1,20 @@
-context("gtf.to.genemodel")
+context("GTF_to_GRangesList")
 
-test_that("gtf.to.genemodel yields correct results", {
-  
-  gtf.file = system.file("extdata", "genes.gtf", package = "ConsensusPeaks")
-  
+test_that("GTF_to_GRangesList yields correct results", {
+
+  gtf = system.file("extdata", "genes.gtf", package = "ConsensusPeaks")
+
   # Basic input
-  gtf.gr = gtf.to.genemodel(gtf.file)
+  gtf.gr = GTF_to_GRangesList(gtf)
   expect_named(
-    gtf.gr, 
+    gtf.gr,
     c("ENSG00000178951.9",
       "ENSG00000185129.7"))
- 
+
   # Selecting transcript
-  gtf.transcript = gtf.to.genemodel(
-    gtf.file,
-    gene.or.transcript = "transcript")
+  gtf.transcript = GTF_to_GRangesList(
+    gtf,
+    gene_or_transcript = "transcript")
   expect_named(
     gtf.transcript,
     c("ENST00000322357.9",
@@ -23,48 +23,48 @@ test_that("gtf.to.genemodel yields correct results", {
       "ENST00000505703.2",
       "ENST00000601588.1",
       "ENST00000651386.1"))
-  
+
   # Selecting chromosome
-  gtf.chr = gtf.to.genemodel(
-    gtf.file,
-    select.chrs = "chr5")
+  gtf.chr = GTF_to_GRangesList(
+    gtf,
+    select_chrs = "chr5")
   expect_named(
     gtf.chr,
     c("ENSG00000185129.7")
   )
-  
+
   # Selecting strand
-  gtf.pos = gtf.to.genemodel(
-    gtf.file,
-    select.strand = "+")
+  gtf.pos = GTF_to_GRangesList(
+    gtf,
+    select_strand = "+")
   expect_named(
     gtf.pos,
     c("ENSG00000185129.7")
   )
-  
-  gtf.neutral = gtf.to.genemodel(
-    gtf.file,
-    select.strand = ".")
+
+  gtf.neutral = GTF_to_GRangesList(
+    gtf,
+    select_strand = "*")
   expect_named(
-    gtf.neutral, 
+    gtf.neutral,
     c("ENSG00000178951.9",
       "ENSG00000185129.7"))
-  
+
   # Selecting gene_id
-  gtf.gene.select = gtf.to.genemodel(
-    gtf.file,
-    select.ids = "ENSG00000178951.9")
+  gtf.gene.select = GTF_to_GRangesList(
+    gtf,
+    select_ids = "ENSG00000178951.9")
   expect_named(
-    gtf.gene.select, 
+    gtf.gene.select,
     c("ENSG00000178951.9"))
-  
+
   # Selecting transcript_id
-  gtf.transcript.select = gtf.to.genemodel(
-    gtf.file,
-    gene.or.transcript = 'transcript',
-    select.ids = "ENST00000322357.9")
+  gtf.transcript.select = GTF_to_GRangesList(
+    gtf,
+    gene_or_transcript = 'transcript',
+    select_ids = "ENST00000322357.9")
   expect_named(
-    gtf.transcript.select, 
+    gtf.transcript.select,
     c("ENST00000322357.9"))
-  
+
 })
