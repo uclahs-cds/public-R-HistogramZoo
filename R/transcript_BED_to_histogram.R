bbc03d47b2cc29dfefb8146a4f7f751b5b9424c0
helen-zhu <helen.m.zhu@gmail.com>
2022-07-21 00:12:23 -0400

    refactor: transcript_BED_to_histogram, update everything to snakecase

--- R/transcript_BED_to_histogram.R
+++ R/transcript_BED_to_histogram.R
@@ -2,73 +2,73 @@
 #'
 #' @param filenames A vector of BED filenames. The `name` column of the BED files must indicate gene or transcript name
 #' @param n_fields Number of columns in the BED file that conform to BED file standards
-#' @param gtf.file A GTF file
-#' @param histogram.bin.size The bin size (base-pairs) to bin signal into a histogram
-#' @param ... Additional parameters to be passed into gtf.to.genemodel
+#' @param gtf A GTF file
+#' @param histogram_bin_size The bin size (base-pairs) to bin signal into a histogram
+#' @param ... Additional parameters to be passed into GTF_to_GRangesList
 #'
 #' @return A list consisting of a list of histograms, a list of gene models and the histogram bin size
 #'
 #' @examples \dontrun{
 #' file.directory = system.file("extdata", "rna_bedfiles", package = "ConsensusPeaks")
 #' filenames = file.path(file.directory, paste0("Sample.", 1:20, ".bed"))
-#' gtf.file = system.file("extdata", "genes.gtf", package = "ConsensusPeaks")
+#' gtf = system.file("extdata", "genes.gtf", package = "ConsensusPeaks")
 #'
-#' histograms = TranscriptBEDToHistogram(
+#' histograms = transcript_BED_to_histogram(
 #' filenames = filenames,
 #' n_fields = 12,
-#' gtf.file = gtf.file,
-#' gene.or.transcript = "gene",
-#' histogram.bin.size = 10)
+#' gtf = gtf,
+#' gene_or_transcript = "gene",
+#' histogram_bin_size = 10)
 #' }
 #'
 #' @export
-TranscriptBEDToHistogram = function(
+transcript_BED_to_histogram = function(
   filenames,
   n_fields = c(4, 6, 12),
-  gtf.file = NULL,
-  histogram.bin.size = 1,
-  gene.or.transcript = c("gene", "transcript"),
-  select.strand = c("*", "+", "-"),
-  select.chrs = NULL,
-  select.ids = NULL,
+  gtf = NULL,
+  histogram_bin_size = 1,
+  gene_or_transcript = c("gene", "transcript"),
+  select_strand = c("*", "+", "-"),
+  select_chrs = NULL,
+  select_ids = NULL,
   ...
 ){
 
   peaks = lapply(filenames, function(filename){
     segs = valr::read_bed( filename, n_fields = n_fields, comment = "#")
     if(n_fields == 12){ segs = valr::bed12_to_exons( segs ) }
-    segs.gr = GenomicRanges::makeGRangesFromDataFrame( segs, keep.extra.columns = T )
-    segs.gr = base0.to.base1(segs.gr)
-    segs.gr = S4Vectors::split(segs.gr, f = segs.gr$name)
-    segs.gr = GenomicRanges::reduce(segs.gr)
-    unlist(segs.gr)
+    segs_gr = GenomicRanges::makeGRangesFromDataFrame( segs, keep.extra.columns = T )
+    segs_gr = base0.to.base1(segs_gr)
+    segs_gr = S4Vectors::split(segs_gr, f = segs_gr$name)
+    segs_gr = GenomicRanges::reduce(segs_gr)
+    unlist(segs_gr)
   })
   peaks = do.call(c, peaks)
   peaks = split(peaks, f = names(peaks))
 
-  regions = gtf.to.genemodel(
-    gtf.file = gtf.file,
-    gene.or.transcript = gene.or.transcript,
-    select.strand = select.strand,
-    select.chrs = select.chrs,
-    select.ids = select.ids
+  regions = GTF_to_GRangesList(
+    gtf = gtf,
+    gene_or_transcript = gene_or_transcript,
+    select_strand = select_strand,
+    select_chrs = select_chrs,
+    select_ids = select_ids
   )
 
   ids = intersect(names(peaks), names(regions))
   if(length(ids) == 0) warning("No intersecting IDs between regions and peaks!")
 
-  histogram.coverage =  vector("list", length(ids))
-  names(histogram.coverage) = ids
+  histogram_coverage =  vector("list", length(ids))
+  names(histogram_coverage) = ids
   for(i in ids){
-    peaks.cov = GenomicRanges::coverage(peaks[[i]])
-    bins = GenomicRanges::tile(x = regions[[i]], width = histogram.bin.size)
+    peaks_cov = GenomicRanges::coverage(peaks[[i]])
+    bins = GenomicRanges::tile(x = regions[[i]], width = histogram_bin_size)
     bins = unlist(bins)
-    GenomeInfoDb::seqlevels(bins) = GenomeInfoDb::seqlevels(peaks.cov)
+    GenomeInfoDb::seqlevels(bins) = GenomeInfoDb::seqlevels(peaks_cov)
     cvg = GenomicRanges::binnedAverage(
       bins = bins,
-      numvar = peaks.cov,
+      numvar = peaks_cov,
       varname = "cvg")
-    histogram.coverage[[i]] <- new_GenomicHistogram(
+    histogram_coverage[[i]] <- new_GenomicHistogram(
       histogram_data = cvg$cvg,
       interval_start = GenomicRanges::start(cvg),
       interval_end = GenomicRanges::end(cvg),
@@ -78,5 +78,5 @@ TranscriptBEDToHistogram = function(
     )
   }
 
-  histogram.coverage
+  return(histogram_coverage)
 }
