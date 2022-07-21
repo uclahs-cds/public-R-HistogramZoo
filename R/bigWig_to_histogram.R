a69300d01f524dee08a922d725d54be720c5fcfd
helen-zhu <helen.m.zhu@gmail.com>
2022-07-21 00:10:43 -0400

    refactor: bigWig_to_histogram, update everything to snakecase

--- R/bigWig_to_histogram.R
+++ R/bigWig_to_histogram.R
@@ -1,12 +1,12 @@
-#' Imports BigWig file and generates histograms
+#' Imports bigWig file and generates histograms
 #'
 #' @param filename Name of bigwig file for import
 #' @param strand The strand of bigwig file from which the data originates. Default ".". If strand is "+" or "-", the strand will also be used to select regions of matching strand.
-#' @param score.threshold A hard threshold for the score of the bigwig file. Scores higher than the threshold will be used in the computation of the histogram.
+#' @param score_threshold A hard threshold for the score of the bigwig file. Scores higher than the threshold will be used in the computation of the histogram.
 #' @param regions A GRanges object to select regions of interest
-#' @param gtf.file A GTF file to select regions of interest
-#' @param histogram.bin.size The bin size (base-pairs) to bin signal into a histogram
-#' @param ... Additional parameters to be passed into gtf.to.genemodel
+#' @param gtf A GTF file to select regions of interest
+#' @param histogram_bin_size The bin size (base-pairs) to bin signal into a histogram
+#' @param ... Additional parameters to be passed into GTF_to_GRangesList
 #'
 #' @return A list consisting of a list of histograms, a list of gene models and the histogram bin size
 #'
@@ -18,29 +18,29 @@
 #'                  end = c(18000, 19600)),
 #' strand = "*")
 #'
-#' histograms = BigWigToHistogram(
+#' histograms = bigWig_to_histogram(
 #' filename = filename,
 #' regions = regions,
-#' histogram.bin.size = 10)
+#' histogram_bin_size = 10)
 #' }
 #'
 #'
 #' @export
-BigWigToHistogram = function(
+bigWig_to_histogram = function(
   filename,
   strand  = c("*", "+", "-"),
-  score.threshold = 0,
+  score_threshold = 0,
   regions = NULL,
-  gtf.file = NULL,
-  histogram.bin.size = 50,
+  gtf = NULL,
+  histogram_bin_size = 50,
   ...
 ){
 
   # Load BigWig file
   bigwig = valr::read_bigwig(filename, set_strand = strand)
-  bigwig.gr = GenomicRanges::makeGRangesFromDataFrame(bigwig, keep.extra.columns = T)
-  bigwig.gr = bigwig.gr[bigwig.gr$score > score.threshold]
-  bigwig.coverage = GenomicRanges::coverage(bigwig.gr, weight = "score")
+  bigwig_gr = GenomicRanges::makeGRangesFromDataFrame(bigwig, keep.extra.columns = T)
+  bigwig_gr = bigwig_gr[bigwig_gr$score > score_threshold]
+  bigwig_coverage = GenomicRanges::coverage(bigwig_gr, weight = "score")
 
   # Loading regions
   if(!is.null(regions)){
@@ -49,21 +49,23 @@ BigWigToHistogram = function(
     }
     ids = if(!is.null(regions$name)) regions$name else generate.identifiers(regions)
     regions = S4Vectors::split(regions, f = ids)
-  } else if(!is.null(gtf.file)){
-    regions = gtf.to.genemodel(
-      gtf.file = gtf.file,
-      select.strand = if(strand %in% c("+", "-")) strand else NULL,
+  } else if(!is.null(gtf)){
+    regions = GTF_to_GRangesList(
+      gtf = gtf,
+      select_strand = if(strand %in% c("+", "-")) strand else NULL,
       ...)
   } else {
-    regions = GenomicRanges::reduce(bigwig.gr)
+    regions = GenomicRanges::reduce(bigwig_gr)
     ids = generate.identifiers(regions)
     regions = S4Vectors::split(regions, f = ids)
   }
 
   # Calculating Coverage
-  coverage.to.histogram(
-    coverage.rle = bigwig.coverage,
-    regions = regions,
-    histogram.bin.size = histogram.bin.size
+  return(
+    coverage_to_histogram(
+      coverage = bigwig_coverage,
+      regions = regions,
+      histogram_bin_size = histogram_bin_size
+    )
   )
 }
