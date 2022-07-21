b41f288a85d83373ab35042bd1ab4537af0e58d9
helen-zhu <helen.m.zhu@gmail.com>
2022-07-21 00:13:23 -0400

    test: updating bigWig_to_histogram tests to fit oop

--- tests/testthat/test-bigWig_to_histogram.R
+++ tests/testthat/test-bigWig_to_histogram.R
@@ -1,11 +1,11 @@
-context("Testing input and parameter variations to bigwig.to.histogram")
+context("Testing input and parameter variations to bigWig_to_histogram")
 
 filename = system.file("extdata", "bigwigs", "test.bw", package = "ConsensusPeaks")
 gtf = system.file("extdata", "genes.gtf",  package = "ConsensusPeaks")
 
-strand = "."
-score.threshold = 0
-histogram.bin.size = 5
+strand = "*"
+score_threshold = 0
+histogram_bin_size = 5
 
 
 test_that("Pre-specified regions yield the correct result", {
@@ -18,241 +18,175 @@ test_that("Pre-specified regions yield the correct result", {
     strand = "-")
 
   # With pre-specified regions
-  histograms.gr.regions = bigwig.to.histogram(
+  histograms.gr.regions = bigWig_to_histogram(
     filename = filename,
     strand = strand,
-    score.threshold = score.threshold,
+    score_threshold = score_threshold,
     regions = regions,
-    gtf.file = NULL,
-    histogram.bin.size = histogram.bin.size)
-  
+    gtf = NULL,
+    histogram_bin_size = histogram_bin_size)
+
   # Result format
-  expect_length(histograms.gr.regions, 3)
+  expect_length(histograms.gr.regions, 1)
   expect_named(
-    histograms.gr.regions, 
-    c("histogram.coverage","gene.model", "histogram.bin.size"))
-  
-  # Length of histograms
-  expect_length(histograms.gr.regions$histogram.coverage, 1)
-  
+    histograms.gr.regions,
+    "chr19:4043303-4048244:-")
+
   # Number of bins
   expect_equal(
-    length(histograms.gr.regions$histogram.coverage[[1]]), 
-    ceiling(GenomicRanges::width(regions)[1]/histogram.bin.size))
-  
-  # Gene model and histograms length and names
-  expect_equal(
-    length(histograms.gr.regions$histogram.coverage), 
-    length(histograms.gr.regions$gene.model))
-  expect_equal(
-    names(histograms.gr.regions$gene.model), 
-    names(histograms.gr.regions$histogram.coverage))
-  
-  # Names of gene model
-  expect_equal(
-    names(histograms.gr.regions$gene.model)[1], 
-    generate.identifiers(regions)[1])
+    length(histograms.gr.regions[[1]]$histogram_data),
+    ceiling(GenomicRanges::width(regions)[1]/histogram_bin_size))
 
-  # Correct histogram bin size
-  expect_equal(
-    histogram.bin.size, 
-    histograms.gr.regions$histogram.bin.size)
-  
+  # TODO: Check other aspects of the return object are correct
+
 })
 
 test_that("Testing that unspecified regions yield correct results", {
-  
+
   # With no specified regions
-  histograms.no.regions = bigwig.to.histogram(
+  histograms.no.regions = bigWig_to_histogram(
     filename = filename,
     strand = strand,
-    score.threshold = score.threshold,
+    score_threshold = score_threshold,
     regions = NULL,
-    gtf.file = NULL,
-    histogram.bin.size = histogram.bin.size)
+    gtf = NULL,
+    histogram_bin_size = histogram_bin_size)
 
   # Result format
-  expect_length(histograms.no.regions, 3)
+  expect_length(histograms.no.regions, 2)
   expect_named(
-    histograms.no.regions, 
-    c("histogram.coverage","gene.model", "histogram.bin.size"))
-  
-  # Length of histograms
-  expect_length(histograms.no.regions$histogram.coverage, 2)
-  
+    histograms.no.regions,
+    c("chr19:4043399-4043499:*", "chr5:140114049-140114174:*"))
+
   # Selects only regions above 0
-  expect_true(all(histograms.no.regions$histogram.coverage[[1]] > 0))
-  
-  # Gene model and histogram length and names
-  expect_equal(
-    length(histograms.no.regions$histogram.coverage), 
-    length(histograms.no.regions$gene.model))
-  expect_equal(
-    names(histograms.no.regions$gene.model), 
-    names(histograms.no.regions$histogram.coverage))
-  
-  # Names of gene model
-  expect_named(
-    histograms.no.regions$gene.model, 
-    c("chr19:4043399-4043499","chr5:140114049-140114174")) 
-  
-  # Correct histogram bin size
-  expect_equal(
-    histogram.bin.size, 
-    histograms.no.regions$histogram.bin.size)
-  
+  expect_true(all(histograms.no.regions[[1]]$histogram_data > 0))
+
 })
 
 test_that("Testing that importing a GTF file yields correct results", {
 
   # With a GTF file
-  histograms.gtf = bigwig.to.histogram(
+  histograms.gtf = bigWig_to_histogram(
     filename = filename,
     strand = strand,
-    score.threshold = score.threshold,
+    score_threshold = score_threshold,
     regions = NULL,
-    gtf.file = gtf,
-    histogram.bin.size = histogram.bin.size)
-  
+    gtf = gtf,
+    histogram_bin_size = histogram_bin_size)
+
   # Result format
-  expect_length(histograms.gtf, 3)
+  expect_length(histograms.gtf, 2)
   expect_named(
-    histograms.gtf, 
-    c("histogram.coverage","gene.model", "histogram.bin.size"))
-  
-  # Length of histograms
-  expect_length(histograms.gtf$histogram.coverage, 2)
-  
-  # Gene model length and length of histograms
-  expect_equal(
-    length(histograms.gtf$histogram.coverage), 
-    length(histograms.gtf$gene.model))
-  expect_equal(
-    names(histograms.gtf$gene.model), 
-    names(histograms.gtf$histogram.coverage))
-  
-  # Takes the gene names
-  expect_named(
-    histograms.gtf$gene.model, 
-    c("ENSG00000178951.9", "ENSG00000185129.7")) 
-  
-  # Correct histogram bin size
-  expect_equal(
-    histogram.bin.size, 
-    histograms.gtf$histogram.bin.size)
+    histograms.gtf,
+    c("ENSG00000178951.9","ENSG00000185129.7"))
 
 })
 
 test_that("Testing that varying bin size yields correct results", {
-  
+
   regions = GenomicRanges::GRanges(
     seqnames = "chr19",
     IRanges::IRanges(start = c(4043303),
                      end = c(4048244)),
     strand = "-")
-  
-  histograms.5 = bigwig.to.histogram(
+
+  histograms.5 = bigWig_to_histogram(
     filename = filename,
     strand = strand,
-    score.threshold = score.threshold,
+    score_threshold = score_threshold,
     regions = regions,
-    gtf.file = NULL,
-    histogram.bin.size = 5)
-  
+    gtf = NULL,
+    histogram_bin_size = 5)
+
   expect_equal(
-    length(histograms.5$histogram.coverage[[1]]), 
+    length(histograms.5[[1]]$histogram_data),
     ceiling(GenomicRanges::width(regions)[1]/5))
 
-  histograms.10 = bigwig.to.histogram(
+  histograms.10 = bigWig_to_histogram(
     filename = filename,
     strand = strand,
-    score.threshold = score.threshold,
+    score_threshold = score_threshold,
     regions = regions,
-    gtf.file = NULL,
-    histogram.bin.size = 10)
-  
+    gtf = NULL,
+    histogram_bin_size = 10)
+
   expect_equal(
-    length(histograms.10$histogram.coverage[[1]]), 
+    length(histograms.10[[1]]$histogram_data),
     ceiling(GenomicRanges::width(regions)[1]/10))
-  
+
   # Creating bins
-  bins.5 = GenomicRanges::tile(x = histograms.5$gene.model[[1]], width = 5)
-  bins.5 = unlist(bins.5)
-  bins.5 = GenomicRanges::width(bins.5)
-  
-  bins.10 = GenomicRanges::tile(x = histograms.10$gene.model[[1]], width = 10)
-  bins.10 = unlist(bins.10)
-  bins.10 = GenomicRanges::width(bins.10)
-  
+  bins.5 = histograms.5[[1]]$interval_end - histograms.5[[1]]$interval_start + 1
+  bins.10 = histograms.10[[1]]$interval_end - histograms.10[[1]]$interval_start + 1
+
   # Checking that the binnedAverage add up to the same thing
   expect_equal(
-    sum(bins.5*histograms.5$histogram.coverage[[1]]), 
-    sum(bins.10*histograms.10$histogram.coverage[[1]]))
-  
+    sum(bins.5*histograms.5[[1]]$histogram_data),
+    sum(bins.10*histograms.10[[1]]$histogram_data))
+
 })
 
 test_that("Testing that selecting for strand yields correct results", {
 
-  histograms.pos = bigwig.to.histogram(
+  histograms.pos = bigWig_to_histogram(
     filename = filename,
     strand = "+",
-    score.threshold = score.threshold,
+    score_threshold = score_threshold,
     regions = NULL,
-    gtf.file = gtf,
-    histogram.bin.size = histogram.bin.size)
-  
+    gtf = gtf,
+    histogram_bin_size = histogram_bin_size)
+
   expect_named(
-    histograms.pos$gene.model, 
-    "ENSG00000185129.7") 
-  
-  histograms.neg = bigwig.to.histogram(
+    histograms.pos,
+    "ENSG00000185129.7")
+
+  histograms.neg = bigWig_to_histogram(
     filename = filename,
     strand = "-",
-    score.threshold = score.threshold,
+    score_threshold = score_threshold,
     regions = NULL,
-    gtf.file = gtf,
-    histogram.bin.size = histogram.bin.size)
-  
+    gtf = gtf,
+    histogram_bin_size = histogram_bin_size)
+
   expect_named(
-    histograms.neg$gene.model,
+    histograms.neg,
     "ENSG00000178951.9"
   )
 
-  histograms.neutral = bigwig.to.histogram(
+  histograms.neutral = bigWig_to_histogram(
     filename = filename,
     strand = ".",
-    score.threshold = score.threshold,
+    score_threshold = score_threshold,
     regions = NULL,
-    gtf.file = gtf,
-    histogram.bin.size = histogram.bin.size)
-  
+    gtf = gtf,
+    histogram_bin_size = histogram_bin_size)
+
   expect_named(
-    histograms.neutral$gene.model,
+    histograms.neutral,
     c("ENSG00000178951.9","ENSG00000185129.7")
   )
 
 })
 
 test_that("Testing that varying the score threshold yields correct results", {
-  
-  histograms.score.1 = bigwig.to.histogram(
+
+  histograms.score.1 = bigWig_to_histogram(
     filename = filename,
     strand = strand,
-    score.threshold = 1,
+    score_threshold = 1,
     regions = NULL,
-    gtf.file = NULL,
-    histogram.bin.size = histogram.bin.size)
-  
-  expect_true(all(histograms.score.1$histogram.coverage[[1]] > 1))
-  
-  histograms.score.2 = bigwig.to.histogram(
+    gtf = NULL,
+    histogram_bin_size = histogram_bin_size)
+
+  expect_true(all(histograms.score.1[[1]]$histogram_data > 1))
+
+  histograms.score.2 = bigWig_to_histogram(
     filename = filename,
     strand = strand,
-    score.threshold = 2,
+    score_threshold = 2,
     regions = NULL,
-    gtf.file = NULL,
-    histogram.bin.size = histogram.bin.size)
-  
-  expect_true(all(histograms.score.2$histogram.coverage[[1]] > 2))
-  
+    gtf = NULL,
+    histogram_bin_size = histogram_bin_size)
+
+  expect_true(all(histograms.score.2[[1]]$histogram_data > 2))
+
 })
