59637eb04c8af5f55c8562c1352d5069803061cd
helen-zhu <helen.m.zhu@gmail.com>
2022-07-23 17:51:55 -0400

    test: updating Histogram tests, adding tests for GenomicHistogram

--- tests/testthat/test-Histogram-Class.R
+++ tests/testthat/test-Histogram-Class.R
@@ -2,7 +2,7 @@ context("Histogram-class")
 
 test_that("Generating a valid Histogram object", {
 
-  x = Histogram(
+  x <- Histogram(
     histogram_data = c(1, 2, 3, 4, 3, 3, 1)
   )
   
@@ -57,7 +57,7 @@ test_that("Generating an invalid Histogram object", {
 
 test_that("Histogram-class methods", {
 
-  x = Histogram(
+  x <- Histogram(
     histogram_data = c(1, 2, 3, 4, 3, 3, 1)
   )
   
@@ -69,14 +69,14 @@ test_that("Histogram-class methods", {
   expect_length(x , 7)
   
   # Extract Indices
-  x_subset = x[2:5]
+  x_subset <- x[2:5]
   expect_equal(
     x_subset$histogram_data,
     c(2, 3, 4, 3)
   )
   
   # reassign_ids
-  x_reassign = reassign_region_id(x, "TEST")
+  x_reassign <- reassign_region_id(x, "TEST")
   expect_equal(x_reassign$region_id, "TEST")
   
 })
