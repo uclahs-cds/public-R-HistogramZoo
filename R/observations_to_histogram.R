58cff6410c9ac1a6515e839b21175e8329fc0a13
helen-zhu <helen.m.zhu@gmail.com>
2022-07-24 00:24:20 -0400

    refactor: simplified observations_to_histogram to fit a more integer-based observation system

--- R/observations_to_histogram.R
+++ R/observations_to_histogram.R
@@ -1,9 +1,7 @@
 #' Take a vector of values and get the histogram for integer breaks
 #'
 #' @param x A vector of observations
-#' @param histogram_bin_width Maximum size of histogram bin, assuming a base 1 system
-#' @param right Logical, indicating if histogram bins should be closed on the right. If FALSE, bins will be closed on the left.
-#' @param include_endpoint Whether the boundary value (highest or lowest) should be counted in the bins.
+#' @param histogram_bin_width Size of histogram bin, assuming a base 1 system
 #'
 #' @return A Histogram object representing counts of the data
 #'
@@ -12,44 +10,38 @@
 #' @example \dontrun{
 #' observations_to_histogram(sample(1:10, 100, replace = T))
 #' }
-observations_to_histogram = function(x, histogram_bin_width = 1, right = TRUE, include_endpoint = TRUE) {
+observations_to_histogram = function(x, histogram_bin_width = 1) {
   
   # Error checking
   stopifnot(is.numeric(x))
   stopifnot(is.numeric(histogram_bin_width))
-  stopifnot(is.logical(right))
-  stopifnot(is.logical(include_endpoint))
   
   # Creating intervals
-  a = floor(min(x))
-  b = ceiling(max(x))
-  histogram_range = b-a+1
-  length_out = ceiling(histogram_range/histogram_bin_width)+1
-  breaks = seq(a, b, length.out = length_out)
-  breaks = round(breaks)
+  a <- floor(min(x))
+  b <- ceiling(max(x))
+  breaks <- seq(a - 1, b, by = histogram_bin_width)
+  breaks <- unique(c(breaks, b))
   
   # Histogram data
-  hist_data = table(
+  hist_data <- table(
     cut(x, 
         breaks = breaks, 
-        right = right, 
-        include.lowest = include_endpoint)
+        right = TRUE, 
+        include.lowest = TRUE
+        )
   )
   
   # Generating endpoints
-  if(right){
-    endpoints = index_to_start_end(breaks, right = TRUE)
-  } else{
-    endpoints = index_to_start_end(breaks, right = FALSE)
-  }
+  breaks <- c(a, breaks[breaks != (a-1)])
+  endpoints <- index_to_start_end(breaks, right = TRUE)
   
   # Generating a new Histogram
   return(
     new_Histogram(
-      histogram_data = hist_data, 
-      interval_start = endpoints$start, 
-      interval_end = endpoints$end, 
-      region_id = NULL
+      histogram_data = as.numeric(hist_data), 
+      interval_start = as.integer(endpoints$start), 
+      interval_end = as.integer(endpoints$end), 
+      region_id = paste0(endpoints$start[1], "-", endpoints$end[length(hist_data)])
     )
   )
 }
