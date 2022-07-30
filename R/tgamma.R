0071288433942b388407d456c97c43ef3303e0dd
helen-zhu <helen.m.zhu@gmail.com>
2022-07-29 23:36:17 -0400

    docs: added docs

--- R/tgamma.R
+++ R/tgamma.R
@@ -1,61 +1,59 @@
 
 #' truncated gamma
+#' 
+#' @param x numeric vector of quantiles
+#' @param shape shape parameter of gamma distribution
+#' @param rate rate parameter of gamma distribution, default 1
+#' @param a distribution start
+#' @param b distribution end
+#' @param ... additional parameters to be passed to truncdist::dtrunc
 #'
-#' @param x TODO
-#' @param shape TODO
-#' @param rate TODO
-#' @param a TODO
-#' @param b TODO
-#' @param ... TODO
-#'
-#' @return TODO
+#' @return density of distribution
 #' @export
 dtgamma <- function(x, shape, rate = 1, a = 0, b = Inf, ...) {
   truncdist::dtrunc(x, spec = 'gamma', a = a, b = b, shape = shape, rate = rate, ...)
 }
 
 #' truncated gamma
+#' 
+#' @param x numeric vector of quantiles
+#' @param shape shape parameter of gamma distribution
+#' @param rate rate parameter of gamma distribution, default 1
+#' @param a distribution start
+#' @param b distribution end
+#' @param ... additional parameters to be passed to truncdist::dtrunc
 #'
-#' @param q TODO
-#' @param shape TODO
-#' @param rate TODO
-#' @param a TODO
-#' @param b TODO
-#' @param ... TODO
-#'
-#' @return TODO
+#' @return distribution function
 #' @export
 ptgamma <- function(q, shape, rate = 1, a = 0, b = Inf, ...) {
   truncdist::ptrunc(q, spec = 'gamma', a = a, b = b, shape = shape, rate = rate, ...)
 }
 
-
 #' truncated gamma
+#' 
+#' @param x numeric vector of quantiles
+#' @param shape shape parameter of gamma distribution
+#' @param rate rate parameter of gamma distribution, default 1
+#' @param a distribution start
+#' @param b distribution end
+#' @param ... additional parameters to be passed to truncdist::dtrunc
 #'
-#' @param n TODO
-#' @param shape TODO
-#' @param rate TODO
-#' @param a TODO
-#' @param b TODO
-#' @param ... TODO
-#'
-#' @return TODO
+#' @return random deviates
 #' @export
 rtgamma <- function(n, shape, rate = 1, a = 0, b = Inf, ...) {
   truncdist::rtrunc(n, spec = 'gamma', a = a, b = b, shape = shape, rate = rate, ...)
 }
 
-
 #' truncated gamma
+#' 
+#' @param x numeric vector of quantiles
+#' @param shape shape parameter of gamma distribution
+#' @param rate rate parameter of gamma distribution, default 1
+#' @param a distribution start
+#' @param b distribution end
+#' @param ... additional parameters to be passed to truncdist::dtrunc
 #'
-#' @param p TODO
-#' @param shape TODO
-#' @param rate TODO
-#' @param a TODO
-#' @param b TODO
-#' @param ... TODO
-#'
-#' @return TODO
+#' @return quantile function
 #' @export
 qtgamma <- function(p, shape, rate = 1, a = 0, b = Inf, ...) {
   truncdist::qtrunc(p, spec = 'gamma', a = a, b = b, shape = shape, rate = rate, ...)
