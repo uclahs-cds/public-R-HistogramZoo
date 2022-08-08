ac4c535c0d855bbc593f7b7f839d34c66cb8271e
helen-zhu <helen.m.zhu@gmail.com>
2022-08-07 21:28:17 -0400

    refactor: updated docs, updated function to loop through coverage_to_histogram

--- R/genome_BED_to_histogram.R
+++ R/genome_BED_to_histogram.R
@@ -1,11 +1,12 @@
 
-#' Generate Histograms from BED files
+#' Generate GenomicHistogram objects from BED files
 #'
-#' @param filenames A vector of BED filenames. The `name` column of the BED files must indicate gene or transcript name
-#' @param n_fields Number of columns in the BED file that conform to BED file standards
+#' @param filenames A vector of BED filenames. The `name` column of the BED files must indicate gene or transcript name.
+#' @param n_fields Number of columns in the BED file that conform to BED file standards. Default 4
 #' @param regions A GRanges or GRangesList object representing regions of interest defining histograms, a GRangesList object allows for the specification of non-continuous segments, default NULL - regions are taken as the set of elements with nonzero coverage
-#' @param histogram_bin_size The bin size (base-pairs) to bin signal into a histogram
-#' @param allow_overlapping_segments_per_sample logical, if FALSE, overlapping segments in the same file will be de-duplicated in the coverage calculation, if TRUE, they will be taken as separate input, default FALSE
+#' @param histogram_bin_size The bin size (base-pairs) to bin signal into a histogram. Default 1
+#' @param allow_overlapping_segments_per_sample logical, if FALSE, overlapping segments in the same file will be de-duplicated in the coverage calculation, 
+#' if TRUE, they will be taken as separate input, default FALSE
 #'
 #' @return A list of GenomicHistogram objects
 #' @export
@@ -47,7 +48,8 @@ genome_BED_to_histogram = function(
   # Formatting/generating regions, output: a GRangesList object defining regions
   if(inherits(regions, "CompressedGRangesList")){
     if(is.null(names(regions))){
-      names(regions) <- generate_identifiers( unlist( range(regions) ) )
+      ids <- generate_identifiers( unlist( range(regions) ) )
+      names(regions) <- ids
     }
   } else if (inherits(regions, "GRanges")){
     ids <- if(!is.null(regions$name)) regions$name else generate_identifiers(regions)
@@ -57,13 +59,21 @@ genome_BED_to_histogram = function(
     ids <- generate_identifiers(regions)
     regions <- S4Vectors::split(regions, f = ids)
   }
-
-  # Calculating Coverage
+  
+  # Sort regions
+  regions = sort(regions)
+  
+  # Generating Histogram objects
   return(
-    coverage_to_histogram(
-      coverage = coverage,
-      regions = regions,
-      histogram_bin_size = histogram_bin_size
+    structure(
+      lapply(names(regions), function(id) ConsensusPeaks:::coverage_to_histogram(
+        region = regions[[id]],
+        region_id = id,
+        coverage = coverage,
+        histogram_bin_size = histogram_bin_size
+        )
+      ),
+      names = names(regions)
     )
   )
 }
