b0c7b808fd8a723655f47dc981bf12851136128a
helen-zhu <helen.m.zhu@gmail.com>
2022-08-09 23:56:07 -0400

    cleaning up inst folder

--- inst/oop_example.R
+++ inst/oop_example.R
@@ -49,8 +49,8 @@ create_coverageplot(
 
 
 x = GenomicHistogram(
-  c(0, 0, 1, 2, 3, 2, 1, 2, 3, 4, 5, 3, 1, 0), 
-  chr = "chr1", 
+  c(0, 0, 1, 2, 3, 2, 1, 2, 3, 4, 5, 3, 1, 0),
+  chr = "chr1",
   strand = c("-"))
 
 results = segment_and_fit(x, eps = 0.005)
@@ -62,8 +62,8 @@ create_coverageplot(
 )
 
 x = GenomicHistogram(
-  runif(12), 
-  interval_start = seq(2, 120, 10), 
-  interval_end = seq(5, 120, 10), 
-  chr = "chr1", 
+  runif(12),
+  interval_start = seq(2, 120, 10),
+  interval_end = seq(5, 120, 10),
+  chr = "chr1",
   strand = "-")
