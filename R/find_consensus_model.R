70eb8d7dd6b78fbb4383afa87b352c88deb20ed5
helen-zhu <helen.m.zhu@gmail.com>
2022-08-18 00:26:43 -0400

    refactor: updating find_local_optima to consider flat regions

---
+++ R/find_consensus_model.R
@@ -0,0 +1,16 @@
+
+#' Methods for voting for a consensus model based on the metrics of fit_distributions
+#'
+#' @param model_fit 
+#' @param method 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+find_consensus_model <- function(
+    model_fit,
+    method = c()
+){
+  
+}
