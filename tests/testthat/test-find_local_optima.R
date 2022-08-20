6317b96d0d7e9f708a6dd70619bc512e7d9ea9c9
helen-zhu <helen.m.zhu@gmail.com>
2022-08-20 02:07:15 -0400

    test: updated tests for neighbouring local optima and cluster of optima

--- tests/testthat/test-find_local_optima.R
+++ tests/testthat/test-find_local_optima.R
@@ -9,12 +9,12 @@ test_that("base case", {
 
 test_that("edge case: entirely flat", {
   x <- rep(1, 5)
-  
+
   # Returns midpoint
   x_optima <- find_local_optima(x, flat_endpoints = F)
   expect_equal(x_optima$min_ind, c(3))
   expect_equal(x_optima$max_ind, NULL)
-  
+
   # Returns endpoint
   x_optima <- find_local_optima(x, flat_endpoints = T)
   expect_equal(x_optima$min_ind, c(1, 5))
@@ -23,22 +23,22 @@ test_that("edge case: entirely flat", {
 
 test_that("edge case: no optima, staircase function", {
   x <- c(1, 1, 1, 2, 2, 3, 3, 3)
-  
+
   # Returns midpoint
   x_optima <- find_local_optima(x, flat_endpoints = F)
   expect_equal(x_optima$min_ind, c(2))
   expect_equal(x_optima$max_ind, c(7))
-  
+
   # Returns endpoint
   x_optima <- find_local_optima(x, flat_endpoints = T)
   expect_equal(x_optima$min_ind, c(1, 3))
   expect_equal(x_optima$max_ind, c(6, 8))
-  
+
 })
 
 test_that("flat elements", {
   x <- c(rep(1, 3), rep(2, 3), rep(3, 3), rep(2, 3), rep(1, 3))
-  
+
   # Returns midpoint
   x_optima <- find_local_optima(x, flat_endpoints = F)
   expect_equal(x_optima$min_ind, c(2, 14))
@@ -50,11 +50,18 @@ test_that("flat elements", {
   expect_equal(x_optima$max_ind, c(7, 9))
 })
 
-test_that("threshold", {
-  x <- c(1, 2, 3, 2, 1, 3, 5, 3, 1)
+test_that("threshold works for basic examples", {
+  x <- c(1, 2, 1, 2, 1, 3, 5, 3, 1)
   x_optima <- find_local_optima(x, threshold = 1)
   expect_equal(x_optima$min_ind, c(1, 9))
   expect_equal(x_optima$max_ind, c(7))
+})
+
+test_that("threshold works for clustered optima", {
+  x <- c(1, 4, 1, 2, 1, 4, 1)
+  x_optima <- find_local_optima(x, threshold = 1)
+  expect_equal(x_optima$min_ind, c(1, 5, 7))
+  expect_equal(x_optima$max_ind, c(2, 6))
 })
 
 test_that("the one case that threshold doesn't work - endpoints", {
