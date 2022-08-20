f98404932174f87909d9c7e4daeb23738aa21982
helen-zhu <helen.m.zhu@gmail.com>
2022-08-20 13:24:50 -0400

    fix: updating find_local_optima to always assign endpoints relative to local optima

--- R/find_local_optima.R
+++ R/find_local_optima.R
@@ -49,19 +49,23 @@ find_local_optima <- function(x, threshold = 0, flat_endpoints = T){
   x_max <- which(diff(x_sign) == -2)+1
   
   # Filter by threshold
-  change_points <- sort(c(x_min, x_max))
-  change_points_vals <- unflat_x$values[change_points]
-  change_points_thresh <- (abs(diff(change_points_vals)) > threshold)
-  change_points_rle <- rle(change_points_thresh)
-  # if we have two consecutive optima points that fail the threshold, deleting them will result in consecutive mins/maxs
-  # therefore, we change the last consecutive optima pair to TRUE to ensure that stretchs of consecutive optima points
-  # will always be of an odd length
-  issue_points <- change_points_rle$lengths %% 2 == 0 & !change_points_rle$value
-  change_points_thresh[cumsum(change_points_rle$lengths)[issue_points]] <- TRUE
-  change_points_keep <- change_points[c(change_points_thresh, TRUE) & c(TRUE, change_points_thresh)]
-  x_min <- x_min[x_min %in% change_points_keep]
-  x_max <- x_max[x_max %in% change_points_keep]
-  
+  optima <- sort(c(x_min, x_max))
+  length_optima <- length(optima)
+  if(length_optima > 1 & threshold > 0){
+    optima_vals <- unflat_x$values[optima]
+    optima_thresh <- (abs(diff(optima_vals)) > threshold)
+    optima_rle <- rle(optima_thresh)
+    # if even numbers of consecutive optima points that fail the threshold, deleting them will result in consecutive mins/maxs
+    # therefore, change the last consecutive optima pair to TRUE to ensure that consecutive optima points
+    # will always be of an odd length
+    issue_points <- optima_rle$lengths %% 2 == 0 & !optima_rle$value
+    optima_thresh[cumsum(optima_rle$lengths)[issue_points]] <- TRUE
+    optima <- optima[c(optima_thresh, TRUE) & c(TRUE, optima_thresh)]
+    length_optima <- length(optima)
+    x_min <- x_min[x_min %in% optima]
+    x_max <- x_max[x_max %in% optima]
+  }
+
   # Filter by (local) threshold
   # x_change_abs <- (abs(x_change) > threshold)
   # x_min <- x_min[x_change_abs[x_min-1] & x_change_abs[x_min]]
@@ -69,46 +73,42 @@ find_local_optima <- function(x, threshold = 0, flat_endpoints = T){
     
   # If flat endpoints, take both ends of the flat segments as indices
   if(flat_endpoints){
-    x_min <- c(unflat_idx[x_min], unflat_idx[x_min - 1] + 1)
-    x_max <- c(unflat_idx[x_max], unflat_idx[x_max - 1] + 1)
+    x_min_adj <- unique(c(unflat_idx[x_min], unflat_idx[x_min - 1] + 1))
+    x_max_adj <- unique(c(unflat_idx[x_max - 1] + 1, unflat_idx[x_max]))
   } else { # Take the midpoint
-    x_min <- rowMeans(cbind(unflat_idx[x_min], unflat_idx[x_min - 1] + 1))
-    x_min <- round(x_min)
-    x_max <- rowMeans(cbind(unflat_idx[x_max], unflat_idx[x_max - 1] + 1))
-    x_max <- round(x_max)
+    x_min_adj <- rowMeans(cbind(unflat_idx[x_min], unflat_idx[x_min - 1] + 1))
+    x_min_adj <- round(x_min_adj)
+    x_max_adj <- rowMeans(cbind(unflat_idx[x_max], unflat_idx[x_max - 1] + 1))
+    x_max_adj <- round(x_max_adj)
   }
   
   # Start & end points
   start_point <- unique(c(1, unflat_idx[1]))
-  end_point <- unique(c(length(x), unflat_idx[length_unflat-1]+1))
-  if(!flat_endpoints) {
+  end_point <- unique(c(unflat_idx[length_unflat-1]+1, length(x)))
+  if(!flat_endpoints) { # Take the midpoint
     start_point <- round(mean(start_point))
     end_point <- round(mean(end_point))
   }
   
-  # Assign start & end points to min/max
-  # This inherently has the same problem as the original find_local_optima
-  # i.e. this is the only place where min/max don't have to alternate in the event
-  # of a threshold (i.e. the if statements don't check if the thresholds hold)
-  if(unflat_x$values[2] > unflat_x$values[1]){
-    x_min <- c(start_point, x_min)
+  # Assign start points to min/max
+  if ( (length_optima > 0 & optima[1] %in% x_max) | 
+       (length_optima == 0 & unflat_x$values[2] > unflat_x$values[1]) ){
+    x_min_adj <- c(start_point, x_min_adj)
   } else {
-    x_max <- c(start_point, x_max)
+    x_max_adj <- c(start_point, x_max_adj)
   }
   
-  if(unflat_x$values[length_unflat] < unflat_x$values[length_unflat - 1]){
-    x_min <- c(x_min, end_point)
+  # Assign end points to min/max
+  if ( (length_optima > 0 & optima[max(length_optima, 1)] %in% x_max) | 
+       (length_optima == 0 & unflat_x$values[length_unflat] < unflat_x$values[length_unflat - 1]) ){
+    x_min_adj <- c(x_min_adj, end_point)
   } else {
-    x_max <- c(x_max, end_point)
+    x_max_adj <- c(x_max_adj, end_point)
   }
-  
-  # Final sort
-  x_min <- sort(unique(x_min))
-  x_max <- sort(unique(x_max))
-  
+
   # Return results
   return( 
-    list('min_ind' = x_min, 'max_ind' = x_max)
+    list('min_ind' = x_min_adj, 'max_ind' = x_max_adj)
   )
   
 }
