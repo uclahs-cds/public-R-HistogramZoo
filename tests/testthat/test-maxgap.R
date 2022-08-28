613e417489be346a127a7bc30dd2f332bc4dbb98
helen-zhu <helen.m.zhu@gmail.com>
2022-08-28 16:48:04 -0400

    test: updating meaningful_gaps

--- tests/testthat/test-maxgap.R
+++ tests/testthat/test-maxgap.R
@@ -1,6 +1,7 @@
 context("max_gap")
 
-test_that("meaningful_gaps_local: basic reproducibility ", {
+test_that("meaningful_gaps_local: basic reproducibility", {
+  
   x <- c(0L, 0L, 1L, 2L, 6L, 11L, 6L, 4L, 2L, 1L, 3L, 5L, 7L, 3L, 1L,
         0L, 0L, 0L, 1L, 3L, 2L, 0L, 0L, 0L)
   seg_points <- c(3, 15, 19, 21)
@@ -16,9 +17,28 @@ test_that("meaningful_gaps_local: basic reproducibility ", {
 
   expect_equal(meaningful_gaps$start, 16)
   expect_equal(meaningful_gaps$end, 18)
+  
+})
+
+test_that("meaningful_gaps_local: edge case", {
+  
+  x <- rep(0, 10)
+  seg_points <- c(1, 10)
+  change_points <- 1:10
+  
+  meaningful_gaps <- meaningful_gaps_local(
+    x,
+    seg_points,
+    change_points,
+    min_gap = 1
+  )
+  
+  expect_equal(nrow(meaningful_gaps), 0)
+  
 })
 
 test_that("find_all_meaningful_gap: basic reproducibility", {
+  
   x <- c(0L, 0L, 1L, 2L, 6L, 11L, 3L, 2L, 0L, 0L, 0L, 2L)
   change_points <- c(1, 2, 3, 4, 5, 7, 8, 9, 11, 12)
 
@@ -26,4 +46,16 @@ test_that("find_all_meaningful_gap: basic reproducibility", {
 
   expect_equal(meaningful_gaps$start, c(1, 9))
   expect_equal(meaningful_gaps$end, c(2, 11))
+  
+})
+
+
+test_that("find_all_meaningful_gap: edge case", {
+  
+  x <- rep(0, 10)
+  change_points <- 1:10
+  
+  meaningful_gaps <- find_all_meaningful_gap(x, change_points)
+  
+  expect_equal(nrow(meaningful_gaps), 0)
 })
