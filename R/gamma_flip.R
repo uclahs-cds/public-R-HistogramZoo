eb98a1a2b37ca6fa6442d23cb79fdc2e0f23bde1
helen-zhu <helen.m.zhu@gmail.com>
2022-09-02 21:21:54 -0400

    refactor: adding an offset and some docs

--- R/gamma_flip.R
+++ R/gamma_flip.R
@@ -1,20 +1,59 @@
-
-#' @export dgamma_flip
-dgamma_flip <- function(x, shape, rate = 1, ...) {
-  stats::dgamma(x = -x, shape = shape, rate = rate, ...)
+#' gamma flip
+#'
+#' @param x numeric vector of quantiles
+#' @param shape shape parameter of gamma distribution
+#' @param rate rate parameter of gamma distribution, default 1
+#' @param offset offset of the right end of the distribution
+#' @inheritParams stats::dgamma
+#' @param ... additional parameters to be passed to stats::dgamma
+#'
+#' @return density of distribution
+#' @export
+dgamma_flip <- function(x, shape, rate = 1, offset = 0, ...) {
+  stats::dgamma(x = offset - x, shape = shape, rate = rate, ...)
 }
 
-#' @export pgamma_flip
-pgamma_flip <- function(q, shape, rate = 1, lower.tail = TRUE, ...) {
-  stats::pgamma(q = -q, shape = shape, rate = rate, lower.tail = !lower.tail, ...)
+#' gamma flip
+#'
+#' @param q numeric vector of quantiles
+#' @param shape shape parameter of gamma distribution
+#' @param rate rate parameter of gamma distribution, default 1
+#' @param offset offset of the right end of the distribution
+#' @inheritParams stats::pgamma
+#' @param ... additional parameters to be passed to stats::pgamma
+#'
+#' @return distribution function
+#' @export
+pgamma_flip <- function(q, shape, rate = 1, offset = 0, lower.tail = TRUE, ...) {
+  stats::pgamma(q = offset - q, shape = shape, rate = rate, lower.tail = !lower.tail, ...)
 }
 
-#' @export rgamma_flip
-rgamma_flip <- function(n, shape, rate = 1, ...) {
-  -stats::rgamma(n = n, shape = shape, rate = rate, ...)
+#' gamma flip
+#'
+#' @param n number of observations
+#' @param shape shape parameter of gamma distribution
+#' @param rate rate parameter of gamma distribution, default 1
+#' @param offset offset of the right end of the distribution
+#' @inheritParams stats::rgamma
+#' @param ... additional parameters to be passed to stats::rgamma
+#'
+#' @return random deviates
+#' @export
+rgamma_flip <- function(n, shape, rate = 1, offset = 0, ...) {
+  offset - stats::rgamma(n = n, shape = shape, rate = rate, ...)
 }
 
-#' @export qgamma_flip
-qgamma_flip <- function(p, shape, rate = 1, lower.tail = TRUE, ...) {
-  -stats::qgamma(p = p, shape = shape, rate = rate, lower.tail = !lower.tail, ...)
+#' gamma flip
+#'
+#' @param p vector of probabilities
+#' @param shape shape parameter of gamma distribution
+#' @param rate rate parameter of gamma distribution, default 1
+#' @param offset offset of the right end of the distribution
+#' @inheritParams stats::qgamma
+#' @param ... additional parameters to be passed to stats::qgamma
+#'
+#' @return quantile
+#' @export
+qgamma_flip <- function(p, shape, rate = 1, offset = 0, lower.tail = TRUE, ...) {
+  offset - stats::qgamma(p = p, shape = shape, rate = rate, lower.tail = !lower.tail, ...)
 }
