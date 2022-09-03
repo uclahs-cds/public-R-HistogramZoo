e4d6f6c42e8ee997163047ef166f0fb3cbfd5420
helen-zhu <helen.m.zhu@gmail.com>
2022-09-03 16:12:05 -0400

    test: adding some tests

--- inst/testing_gamma_flip.R
+++ inst/testing_gamma_flip.R
@@ -24,11 +24,11 @@ hist(rgamma_data, breaks = 100)
 dgamma_data = dgamma(1:20, shape = 10)
 plot(dgamma_data)
 
-x = -rev(1:20)
+x = -c(1:20)
 dgamma_flip_data = dgamma_flip(x, shape = 10)
 plot(x, dgamma_flip_data)
 
-dgamma_flip_data = dgamma_flip(1:20, shape = 10, offset =20)
+dgamma_flip_data = dgamma_flip(rev(0:19), shape = 10, offset =20)
 plot(dgamma_flip_data)
 
 # pgamma
@@ -58,21 +58,71 @@ rtgamma_flip_data = rtgamma_flip(10000, shape = 10, a = 5, b = 20)
 hist(rtgamma_flip_data, breaks = 100)
 
 # dtgamma
-dtgamma_data = dtgamma(1:25, shape = 10, a = 5, b = 20)
+dtgamma_data = dtgamma(5:20, shape = 10, a = 5, b = 20)
 plot(dtgamma_data)
 
-dtgamma_flip_data = dtgamma_flip(1:25, shape = 10, a = 5, b = 20)
+dtgamma_flip_data = dtgamma_flip(5:20, shape = 10, a = 5, b = 20)
 plot(dtgamma_flip_data)
 
 # ptgamma
 ptgamma_data = ptgamma(5:20, shape = 10, a = 5, b = 20)
 plot(ptgamma_data)
 
-x = -c(0:15)+20
-ptgamma_flip_data = ptgamma_flip(x, shape = 10, lower.tail = F, a = 5, b = 20)
+x = rev(5:20)
+ptgamma_flip_data = ptgamma_flip(rev(5:20), shape = 10, lower.tail = F, a = 5, b = 20)
 plot(x, ptgamma_flip_data)
 
 # qgamma
 ptgamma(qtgamma(seq(0, 0.9, 0.1), shape = 10), shape = 10)
 ptgamma(qtgamma(seq(0, 0.9, 0.1), shape = 10, a = 5, b = 20), shape = 10, a = 5, b = 20)
 ptgamma_flip(qtgamma_flip(seq(0, 0.9, 0.1), shape = 10, a = 5, b = 20), shape = 10, a = 5, b = 20)
+
+# Testing fit_distributions on gamma_flip ---------------------------------
+
+set.seed(314)
+histogram_data <- rgamma_flip(10000, shape = 10)
+histogram <- observations_to_histogram(histogram_data)
+histogram_data <- histogram$histogram_data
+
+metric <- c("jaccard", "intersection", "ks", "mse", "chisq")
+res <- fit_distributions(
+  histogram_data,
+  metric = metric,
+  truncated = F,
+  distributions = c("gamma_flip")
+)
+res_summary <- find_consensus_model(res)[['consensus']]
+
+distribution_plotting_data <- lapply(res, function(m) {
+  x <- seq(1, 26, by = 1)
+  dens <- m$dens(x = seq_along(x), mpar = m$par)
+  return(
+    data.frame("fitted" = dens, "labels_x" = 1:26)
+  )
+})
+
+res = segment_and_fit(histogram, distributions = c("gamma_flip"))
+create_coverageplot(res)
+
+histogram_data <- rtgamma_flip(10000, shape = 10, b = 20, a = 5)
+histogram <- observations_to_histogram(histogram_data)
+histogram_data <- histogram$histogram_data
+
+metric <- c("jaccard", "intersection", "ks", "mse", "chisq")
+res <- fit_distributions(
+  histogram_data,
+  metric = metric,
+  truncated = T,
+  distributions = c("gamma_flip")
+)
+res_summary <- find_consensus_model(res)[['consensus']]
+distribution_plotting_data <- lapply(res, function(m) {
+  x <- seq(1, 16, by = 1)
+  dens <- m$dens(x = seq_along(x), mpar = m$par)
+  return(
+    data.frame("fitted" = dens, "labels_x" = 1:16)
+  )
+})
+
+res = segment_and_fit(histogram, distributions = c("gamma_flip"), truncated = T, eps = 1, remove_low_entropy = F)
+create_coverageplot(res)
