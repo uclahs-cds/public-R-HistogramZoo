fd22ecd1038f4ffa13237a47cefbb36160178b3f
helen-zhu <helen.m.zhu@gmail.com>
2022-09-28 23:30:47 -0400

    test: updating trackplot tests for new start and end columns

--- inst/new_trackplot_tests.R
+++ inst/new_trackplot_tests.R
@@ -9,7 +9,9 @@ df = data.frame(
   "row_id_factor" = factor(LETTERS[1:4], levels = LETTERS[1:4]),
   "colour_id" = c("red", "blue", "red", "green"),
   "start" = c(1,3,5,7),
-  "end" = c(2, 4, 6, 8)
+  "end" = c(2, 4, 6, 8),
+  "start_id" = c(1,3,5,7),
+  "end_id" = c(2, 4, 6, 8)
 )
 
 create_trackplot(
@@ -46,4 +48,13 @@ create_trackplot(
   row_id = "row_id_num_one",
   metric_id = "metric_id",
   colour_id = "colour_id"
+)
+
+create_trackplot(
+  track_data = df,
+  row_id = "row_id_num_one",
+  metric_id = "metric_id",
+  colour_id = "colour_id",
+  start_id = "start_id",
+  end_id = "end_id"
 )
