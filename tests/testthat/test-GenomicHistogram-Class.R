a152041da0575615f7d0ceb191b55760413fc4dc
helen-zhu <helen.m.zhu@gmail.com>
2022-10-02 01:08:50 -0400

    test: update tests for Histogram and GenomicHistogram classes

--- tests/testthat/test-GenomicHistogram-Class.R
+++ tests/testthat/test-GenomicHistogram-Class.R
@@ -1,25 +1,33 @@
 context("GenomicHistogram-class")
 
 test_that("Generating a valid Histogram object", {
-  
+
   x <- GenomicHistogram(
     histogram_data = c(1, 2, 3, 4, 3, 3, 1),
     chr = "chr1"
   )
-  
+
   # strand
   expect_equal(x$strand, "*")
-  
+
   # chromosome
   expect_equal(x$chr, "chr1")
-  
+
   # region_id
   expect_equal(x$region_id, "chr1:1-7:*")
-  
+
 })
 
 test_that("Generating an invalid GenomicHistogram object", {
-  
+
+  expect_error(
+    Histogram(
+      runif(10),
+      interval_start = 1:10,
+      interval_end = 1:10 + 1
+    )
+  )
+
   # strand
   expect_error(
     GenomicHistogram(
@@ -27,32 +35,32 @@ test_that("Generating an invalid GenomicHistogram object", {
       strand = "TEST"
     )
   )
-  
+
 })
 
 test_that("GenomicHistogram-class methods", {
-  
+
   x <- GenomicHistogram(
     histogram_data = c(1, 2, 3, 4, 3, 3, 1),
     chr = "chr1"
   )
-  
+
   # Print
   # TODO: Add the expected print output
   expect_invisible(print(x))
-  
+
   # Length
   expect_length(x , 7)
-  
+
   # Extract Indices
   x_subset <- x[2:5]
   expect_equal(
     x_subset$histogram_data,
     c(2, 3, 4, 3)
   )
-  
+
   # reassign_ids
   x_reassign <- reassign_region_id(x, "TEST")
   expect_equal(x_reassign$region_id, "TEST")
-  
+
 })
--- tests/testthat/test-Histogram-Class.R
+++ tests/testthat/test-Histogram-Class.R
@@ -5,27 +5,27 @@ test_that("Generating a valid Histogram object", {
   x <- Histogram(
     histogram_data = c(1, 2, 3, 4, 3, 3, 1)
   )
-  
+
   # Interval starts
   expect_equal(x$interval_start, 1:7)
-  
+
   # Interval ends
-  expect_equal(x$interval_end, 1:7)
-  
+  expect_equal(x$interval_end, 2:8)
+
   # region_ids
-  expect_equal(x$region_id, "1-7")
-  
+  expect_equal(x$region_id, "1-8")
+
 })
 
 test_that("Generating an invalid Histogram object", {
-  
+
   # Invalid data
   expect_error(
     Histogram(
       c(-1, 2, 2, -1)
     )
   )
-  
+
   # Invalid intervals
   expect_error(
     Histogram(
@@ -34,7 +34,15 @@ test_that("Generating an invalid Histogram object", {
       interval_end = 1:10 - 1
     )
   )
-  
+
+  expect_error(
+    Histogram(
+      runif(10),
+      interval_start = 1:10,
+      interval_end = 1:10
+    )
+  )
+
   # Non ordered intervals
   expect_error(
     Histogram(
@@ -43,7 +51,7 @@ test_that("Generating an invalid Histogram object", {
       interval_end = c(3, 1)
     )
   )
-  
+
   # Overlapping intervals
   expect_error(
     Histogram(
@@ -52,7 +60,7 @@ test_that("Generating an invalid Histogram object", {
       interval_end = c(2, 3)
     )
   )
-  
+
 })
 
 test_that("Histogram-class methods", {
@@ -60,23 +68,23 @@ test_that("Histogram-class methods", {
   x <- Histogram(
     histogram_data = c(1, 2, 3, 4, 3, 3, 1)
   )
-  
+
   # Print
   # TODO: Add the expected print output
   expect_invisible(print(x))
-  
+
   # Length
   expect_length(x , 7)
-  
+
   # Extract Indices
   x_subset <- x[2:5]
   expect_equal(
     x_subset$histogram_data,
     c(2, 3, 4, 3)
   )
-  
+
   # reassign_ids
   x_reassign <- reassign_region_id(x, "TEST")
   expect_equal(x_reassign$region_id, "TEST")
-  
+
 })
