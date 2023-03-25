670aab03efb7313a274cebcafb610ca28ff57f66
Stefan Eng <stefaneng@mednet.ucla.edu>
2023-03-24 17:55:55 -0700

    randomized unimodal sim

--- R/simulation.R
+++ R/simulation.R
@@ -1,8 +1,91 @@
+random_unimodal_sim <- function(
+    N = c(25, 500),
+    unif_length = c(6, 25),
+    norm_sd = c(1, 4),
+    gamma_shape = c(1, 4),
+    eps = c(0.05, 0.5)
+  ) {
+  sim_dist <- sample(c('norm', 'unif', 'gamma'), size = 1)
+  # .runif1 <- function(...) runif(n = 1, ...)
+  .sample_unif <- function(x, n = 1) {
+    runif(n, min = x[1], max = x[2])
+  }
+
+  N_sim <- .sample_unif(N)
+  eps_sample <- .sample_unif(eps)
+
+  if (sim_dist == 'norm') {
+    param <- .sample_unif(norm_sd)
+    peak <- rnorm(N_sim, mean = 0, sd = param)
+  } else if (sim_dist == 'gamma') {
+    param <- .sample_unif(gamma_shape)
+    peak <- rgamma(N_sim, scale = 1, shape = param)
+  } else {
+    # Unif
+    param <- .sample_unif(unif_length)
+    peak <- runif(N_sim, min = 0, max = param)
+  }
+
+  histogram_data <- observations_to_histogram(
+    x = peak,
+    histogram_bin_width = 1
+    )
+
+  timing <- system.time({
+    seg_results <- try({
+      segment_and_fit(
+        histogram_data,
+        max_uniform = FALSE,
+        remove_low_entropy = TRUE,
+        eps = eps_sample,
+        truncated_models = FALSE,
+        metric_weights =  sqrt(c(5,4,3,2,1))
+        )
+      }, silent = TRUE)
+  })
+
+  list(
+    N = N_sim,
+    param = param,
+    data = peak,
+    dist = sim_dist,
+    eps = eps_sample,
+    timing = timing,
+    seg_results = seg_results
+    )
+}
+
+  # Set optima_flat_endpoints = FALSE
+  # Background noise change to M: number of uniform
+  # Simulation d: distance between peaks as % of variability
+
+  # Peaks = 1 or 2
+  # N = 25... 500
+  # Params:
+  # Unif: length
+  # Norm: sd
+  # Gamma: shape (skewness)
+  # Distance between peaks d: % overlap of the peaks
+
+  # Alt distribution: Fit semicircle?
+
+
+  # Simulate epsilon from .05 to .5: function of true distribution and initialization points
+  # max_uniform = FALSE
+  # remove_low_entropy = TRUE
+  # Set default of max_uniform to false?
+
+  # Use the square-root of sqrt(c(5,4,3,2,1)) for weights
+  # Determine the best consensus weighting based on the results from the simulation
+
+  # keep consensus method the same
+  # Evaluate performance for each metric individually and as consensus
 general_sim <- function(
     N,
     param1,
     param2,
-    seed = 131313,
+    seed = NULL,
+    histogram_bin_width = 1,
     rfunc = c('rnorm', 'rgamma', 'runif', 'rlnorm'),
     quiet = TRUE, ...) {
   if (!quiet) {
@@ -13,7 +96,8 @@ general_sim <- function(
   set.seed(seed)
 
   histogram_data <- observations_to_histogram(
-    rfunc(N, param1, param2)
+    rfunc(N, param1, param2),
+    histogram_bin_width = histogram_bin_width
     )
 
   segment_args <- c(
