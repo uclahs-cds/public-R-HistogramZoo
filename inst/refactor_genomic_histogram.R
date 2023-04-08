cbd583cf13152ef8aa5187cce21ac8896d129238
helen-zhu <helen.m.zhu@gmail.com>
2023-04-07 21:10:01 -0400

    refactor: coverage_to_histogram to be intron-spanning and have consistent bin_width; addresses #103 #104

--- inst/refactor_genomic_histogram.R
+++ inst/refactor_genomic_histogram.R
@@ -38,7 +38,9 @@ coverage_to_histogram = function(
       bin_width = as.integer(histogram_bin_size),
       region_id = region_id,
       chr = as.character(GenomicRanges::seqnames(cvg))[1],
-      strand = as.character(GenomicRanges::strand(cvg))[1]
+      strand = as.character(GenomicRanges::strand(cvg))[1],
+      intron_start = integer(),
+      intron_end = integer()
     )
   )
 }
@@ -82,11 +84,11 @@ two_segment_region = GRanges(
   strand = "+"
 )
 
-three_segment_region = GRanges(
+multi_intron_region = GRanges(
   seqnames = "chr1",
   IRanges(
-    start = c(1, 21, 31),
-    end = c(10, 30, 40)
+    start = c(1, 6, 9),
+    end = c(4, 7, 20)
   ),
   strand = "+"
 )
@@ -97,7 +99,7 @@ coverage <- GenomicRanges::coverage(
     seqnames = "chr1",
     IRanges(
       start = c(1, 21, 31),
-      end = c(10, 30, 40)
+      end = c(20, 30, 40)
     ),
     strand = "+"
   )
@@ -108,56 +110,132 @@ region_id = "TEST"
 
 # Fixing tiling -----------------------------------------------------------
 
-region <- two_segment_region
-histogram_bin_size <- 4
+coverage_to_histogram_new = function(
+    region,
+    region_id,
+    coverage,
+    histogram_bin_size
+){
 
-region_range <- range(region)
-introns <- setdiff(region_range, region)
-bins <- unlist(GenomicRanges::tile(x = region, width = 1))
-breaks <- seq(1, length(bins), histogram_bin_size)
-breaks <- unique(c(breaks, length(bins)))
+  region_range <- range(region)
+  introns <- setdiff(region_range, region)
+  bins <- unlist(GenomicRanges::tile(x = region, width = 1))
+  breaks <- seq(1, length(bins), histogram_bin_size)
+  breaks <- unique(c(breaks, length(bins)))
+  
+  if(histogram_bin_size == 1){
+    break_start <- breaks
+    break_end <- breaks
+  } else if(length(breaks) == 2){
+    break_start <- 1
+    break_end <- breaks[2]
+  } else {
+    break_start <- breaks[1:(length(breaks)-1)]
+    break_end <- c(breaks[2:(length(breaks)-1)]-1, tail(breaks, n = 1))
+  }
 
-if(length(breaks) == 2){
-  break_start <- 1
-  break_end <- breaks[2]
-} else {
-  break_start <- breaks[1:(length(breaks)-1)]
-  break_end <- c(breaks[2:(length(breaks)-1)]-1, tail(breaks, n = 1))
+  bins <- lapply(seq_along(break_start), function(i) reduce(bins[break_start[i]:break_end[i]]))
+  bins <- GRangesList(bins)
+  names(bins) <- generate_grangeslist_identifiers(bins)
+  bins <- unlist(bins)
+  GenomeInfoDb::seqlevels(bins) <- GenomeInfoDb::seqlevels(coverage)
+
+  cvg <- GenomicRanges::binnedAverage(
+    bins = bins,
+    numvar = coverage,
+    varname = "cvg"
+  )
+
+  cvg <- split(cvg, f = names(cvg))
+  cvg[sapply(cvg, length) > 1] <- GRangesList(sapply(cvg[sapply(cvg, length) > 1], weighted_coverage))
+  cvg <- unlist(cvg)
+  cvg <- sort(cvg)
+  
+  # Generating new histogram
+  return(
+    HistogramZoo:::new_GenomicHistogram(
+      histogram_data = cvg$cvg,
+      interval_start = GenomicRanges::start(cvg),
+      interval_end = GenomicRanges::end(cvg),
+      bin_width = as.integer(histogram_bin_size),
+      region_id = region_id,
+      chr = as.character(GenomicRanges::seqnames(cvg))[1],
+      strand = as.character(GenomicRanges::strand(cvg))[1],
+      intron_start = GenomicRanges::start(introns),
+      intron_end = GenomicRanges::end(introns)
+    )
+  )
 }
 
-bins <- lapply(seq_along(break_start), function(i) reduce(bins[break_start[i]:break_end[i]]))
-bins <- GRangesList(bins)
-names(bins) <- generate_grangeslist_identifiers(bins)
-bins <- unlist(bins)
-GenomeInfoDb::seqlevels(bins) <- GenomeInfoDb::seqlevels(coverage)
+# benchmark ---------------------------------------------------------------
 
-cvg <- GenomicRanges::binnedAverage(
-  bins = bins,
-  numvar = coverage,
-  varname = "cvg"
-)
+compare_functions <- function(region, coverage, region_id, histogram_bin_size){
+  
+  start_time <- Sys.time()
+  old = coverage_to_histogram(
+    region = one_segment_region,
+    coverage = coverage,
+    region_id =  region_id,
+    histogram_bin_size = 1
+  )
+  end_time <- Sys.time()
+  t1 = end_time - start_time
+  
+  start_time <- Sys.time()
+  new = coverage_to_histogram_new(
+      region = one_segment_region,
+      coverage = coverage,
+      region_id =  region_id,
+      histogram_bin_size = 1
+  )
+  end_time <- Sys.time()
+  t2 = end_time - start_time
+  
+  cat("Slow down: ", as.numeric(t2)/as.numeric(t1), "\n")
+  
+  # return(list("New" = t2, "Old" = t1))
+  return(new)
+}
 
-cvg <- split(cvg, f = names(cvg))
-cvg[sapply(cvg, length) > 1] <- GRangesList(sapply(cvg[sapply(cvg, length) > 1], weighted_coverage))
-cvg <- unlist(cvg)
-
-# Add introns to GenomicHistograms
 
 # Test Cases --------------------------------------------------------------
 
 # Bin size 1 - base case
-
-# Checking that the last bin is indeed a different size if length of object is different
-
-# Checking that bins span introns
-
-# Multiple introns per bin
-
-# coverage_to_histogram
-tmp = coverage_to_histogram(
-  region = two_segment_region,
+base_case <- compare_functions(
+  region = one_segment_region,
   coverage = coverage,
   region_id =  region_id,
   histogram_bin_size = 1
 )
 
+# Bin size 2 - base case
+base_case_two <- compare_functions(
+  region = one_segment_region,
+  coverage = coverage,
+  region_id =  region_id,
+  histogram_bin_size = 2
+)
+
+# Checking that the last bin is indeed a different size if length of object is different
+non_divisible_bin_length <- compare_functions(
+  region = one_segment_region,
+  coverage = coverage,
+  region_id =  region_id,
+  histogram_bin_size = 4
+)
+
+# Checking that bins span introns
+base_intron_case <- compare_functions(
+  region = two_segment_region,
+  coverage = coverage,
+  region_id =  region_id,
+  histogram_bin_size = 4
+)
+
+# Multiple introns per bin
+multi_intron_case <- compare_functions(
+  region = multi_intron_region,
+  coverage = coverage,
+  region_id =  region_id,
+  histogram_bin_size = 4
+)
