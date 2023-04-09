59652904226d7348a9fababb9bc6986fc64752b5
helen-zhu <helen.m.zhu@gmail.com>
2023-04-08 23:54:51 -0400

    refactor: draft 1 of refactored coverage_to_histogram

--- inst/refactor_genomic_histogram.R
+++ inst/refactor_genomic_histogram.R
@@ -39,6 +39,8 @@ coverage_to_histogram = function(
       region_id = region_id,
       chr = as.character(GenomicRanges::seqnames(cvg))[1],
       strand = as.character(GenomicRanges::strand(cvg))[1],
+      # The following two lines are the only changes to the original function
+      # as a result of the addition of introns
       intron_start = integer(),
       intron_end = integer()
     )
@@ -58,35 +60,35 @@ generate_grangeslist_identifiers <- function(gr_list){
 }
 
 weighted_coverage <- function(split_coverage){
-  seg_length <- width(split_coverage)
-  gr <- range(split_coverage)
+  seg_length <- IRanges::width(split_coverage)
+  gr <- base::range(split_coverage)
   gr$cvg <- sum( split_coverage$cvg * seg_length) / sum(seg_length)
   return(gr)
 }
 
 # Test Data ---------------------------------------------------------------
 
-one_segment_region = GRanges(
+one_segment_region = GenomicRanges::GRanges(
   seqnames = "chr1",
-  IRanges(
+  IRanges::IRanges(
     start = c(1),
     end = c(10)
   ),
   strand = "+"
 )
 
-two_segment_region = GRanges(
+two_segment_region = GenomicRanges::GRanges(
   seqnames = "chr1",
-  IRanges(
+  IRanges::IRanges(
     start = c(1, 21),
     end = c(10, 30)
   ),
   strand = "+"
 )
 
-multi_intron_region = GRanges(
+multi_intron_region = GenomicRanges::GRanges(
   seqnames = "chr1",
-  IRanges(
+  IRanges::IRanges(
     start = c(1, 6, 9),
     end = c(4, 7, 20)
   ),
@@ -95,11 +97,23 @@ multi_intron_region = GRanges(
 
 # coverage
 coverage <- GenomicRanges::coverage(
-  GRanges(
+  GenomicRanges::GRanges(
     seqnames = "chr1",
-    IRanges(
+    IRanges::IRanges(
       start = c(1, 21, 31),
       end = c(20, 30, 40)
+    ),
+    strand = "+"
+  )
+)
+
+# uneven coverage
+uneven_coverage <- GenomicRanges::coverage(
+  GenomicRanges::GRanges(
+    seqnames = "chr1",
+    IRanges::IRanges(
+      start = c(1, 21, 21),
+      end = c(20, 30, 30)
     ),
     strand = "+"
   )
@@ -116,9 +130,9 @@ coverage_to_histogram_new = function(
     coverage,
     histogram_bin_size
 ){
-
-  region_range <- range(region)
-  introns <- setdiff(region_range, region)
+  
+  region_range <- base::range(region)
+  introns <- GenomicRanges::setdiff(region_range, region)
   bins <- unlist(GenomicRanges::tile(x = region, width = 1))
   breaks <- seq(1, length(bins), histogram_bin_size)
   breaks <- unique(c(breaks, length(bins)))
@@ -133,28 +147,28 @@ coverage_to_histogram_new = function(
     break_start <- breaks[1:(length(breaks)-1)]
     break_end <- c(breaks[2:(length(breaks)-1)]-1, tail(breaks, n = 1))
   }
-
-  bins <- lapply(seq_along(break_start), function(i) reduce(bins[break_start[i]:break_end[i]]))
-  bins <- GRangesList(bins)
+  
+  bins <- lapply(seq_along(break_start), function(i) IRanges::reduce(bins[break_start[i]:break_end[i]]))
+  bins <- GenomicRanges::GRangesList(bins)
   names(bins) <- generate_grangeslist_identifiers(bins)
   bins <- unlist(bins)
   GenomeInfoDb::seqlevels(bins) <- GenomeInfoDb::seqlevels(coverage)
-
+  
   cvg <- GenomicRanges::binnedAverage(
     bins = bins,
     numvar = coverage,
     varname = "cvg"
   )
-
-  cvg <- split(cvg, f = names(cvg))
-  cvg[sapply(cvg, length) > 1] <- GRangesList(sapply(cvg[sapply(cvg, length) > 1], weighted_coverage))
+  
+  cvg <- GenomicRanges::split(cvg, f = names(cvg))
+  cvg[sapply(cvg, length) > 1] <- GenomicRanges::GRangesList(sapply(cvg[sapply(cvg, length) > 1], weighted_coverage))
   cvg <- unlist(cvg)
   cvg <- sort(cvg)
   
   # Generating new histogram
   return(
     HistogramZoo:::new_GenomicHistogram(
-      histogram_data = cvg$cvg,
+      histogram_data = unname(cvg$cvg),
       interval_start = GenomicRanges::start(cvg),
       interval_end = GenomicRanges::end(cvg),
       bin_width = as.integer(histogram_bin_size),
@@ -173,20 +187,20 @@ compare_functions <- function(region, coverage, region_id, histogram_bin_size){
   
   start_time <- Sys.time()
   old = coverage_to_histogram(
-    region = one_segment_region,
+    region = region,
     coverage = coverage,
     region_id =  region_id,
-    histogram_bin_size = 1
+    histogram_bin_size = histogram_bin_size
   )
   end_time <- Sys.time()
   t1 = end_time - start_time
   
   start_time <- Sys.time()
   new = coverage_to_histogram_new(
-      region = one_segment_region,
+      region = region,
       coverage = coverage,
       region_id =  region_id,
-      histogram_bin_size = 1
+      histogram_bin_size = histogram_bin_size
   )
   end_time <- Sys.time()
   t2 = end_time - start_time
@@ -236,6 +250,14 @@ base_intron_case <- compare_functions(
 multi_intron_case <- compare_functions(
   region = multi_intron_region,
   coverage = coverage,
+  region_id =  region_id,
+  histogram_bin_size = 4
+)
+
+# Uneven coverage
+uneven_coverage_case <- compare_functions(
+  region = two_segment_region,
+  coverage = uneven_coverage,
   region_id =  region_id,
   histogram_bin_size = 4
 )
