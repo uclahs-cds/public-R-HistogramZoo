3576ced500028d5dccb002efd51af2577d2c9cc2
helen-zhu <helen.m.zhu@gmail.com>
2023-04-08 23:55:36 -0400

    feat: adding a helper function to name GRangesList objects

--- R/GRanges-utils.R
+++ R/GRanges-utils.R
@@ -1,5 +1,5 @@
 #' Converts a GRanges object from base 0 to base 1
-#' 
+#'
 #' @param gr a GRanges object in base 0
 #' @return a GRanges object in base 1
 base0_to_base1 <- function(gr){
@@ -18,6 +18,22 @@ generate_identifiers <- function(gr){
       GenomicRanges::start(gr), "-",
       GenomicRanges::end(gr), ":",
       GenomicRanges::strand(gr)
+    )
+  )
+}
+
+#' Generates region_ids for GRangesList objects
+#'
+#' @param gr a GRangesList object
+#' @return character identifiers for each GRanges object in a GRangesList
+generate_grangeslist_identifiers <- function(gr_list){
+  gr_ranges <- range(gr_list)
+  return(
+    paste0(
+      GenomicRanges::seqnames(gr_ranges), ":",
+      GenomicRanges::start(gr_ranges), "-",
+      GenomicRanges::end(gr_ranges), ":",
+      GenomicRanges::strand(gr_ranges)
     )
   )
 }
