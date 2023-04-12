605305c29ec5487aedd897b22a5a9cbfcd63c15d
helen-zhu <helen.m.zhu@gmail.com>
2023-04-12 17:15:17 -0400

    fix: removing assumption test that truncated always fits better than full

--- tests/testthat/test-segment_and_fit.R
+++ tests/testthat/test-segment_and_fit.R
@@ -179,7 +179,9 @@ test_that("subfunction: fit_distributions", {
   res2_summary <- summarize_results(res2)
 
   expect_true(res2$truncated_models)
-  expect_true(res2_summary$value[2] > res_summary$value[2])
+
+  # Apparently this is not always true - the values can be quite close if the data is clean
+  # expect_true(res2_summary$value[2] > res_summary$value[2])
 })
 
 test_that("subfunction: find_consensus_model", {
