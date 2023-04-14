e7167b8fd4304a77814fbe76485e1f9bb366e708
Stefan Eng <stefaneng@mednet.ucla.edu>
2023-04-14 07:20:04 -0700

    Update roxygen docs to and fix bin_width

--- R/moments.R
+++ R/moments.R
@@ -1,32 +1,30 @@
-#' Computes the weighted sample standard deviation
+#' Weighted higher order moments
+#'
+#' Computes weighted sample mean, standard deviation, variance, and skewness.
+#' See stats::\link[stats]{weighted.mean}.
 #'
 #' @export
-#' @rdname weighted.sd
+#' @rdname weighted.moments
 weighted.sd <- function(x, ...) UseMethod('weighted.sd')
 
-#' Computes the weighted sample variance
 #' @export
-#' @rdname weighted.sd
+#' @rdname weighted.moments
 weighted.var <- function(x, ...) UseMethod('weighted.var')
 
-#' Computes the weighted sample skewness
 #' @export
-#' @rdname weighted.skewness
+#' @rdname weighted.moments
 weighted.skewness <- function(x, ...) UseMethod('weighted.skewness')
 
-#' Computes the sample moments of a Histogram or GenomicHistogram
-#'
-#' weighted.mean.Histogram computes a the sample variance of a Histogram or GenomicHistogram
-#'
 #' @param x Histogram or GenomicHistogram
 #'
 #' @exportS3Method weighted.mean Histogram
+#' @rdname weighted.moments
 weighted.mean.Histogram <- function(x) {
   midpoints <- (x$interval_end + x$interval_start) / 2
   return(weighted.mean(x = midpoints, w = x$histogram_data))
 }
 
-#' weighted.var.Histogram computes a the sample variance of a Histogram or GenomicHistogram, with option for Sheppard's correction.
+#' Sample variance of a Histogram or GenomicHistogram
 #'
 #' @param x Histogram or GenomicHistogram
 #' @param sheppard Should Sheppard's correction be applied? (subtract h^2/12 from the variance). Should only be applied when bin widths are all the same
@@ -34,21 +32,23 @@ weighted.mean.Histogram <- function(x) {
 #' @rdname weighted.moments
 #' @exportS3Method weighted.var Histogram
 weighted.var.Histogram <- function(x, sheppard = FALSE) {
+  bin_width <- x$interval_end - x$interval_start
   midpoints <- (x$interval_end + x$interval_start) / 2
   adj <- 0
   if (sheppard) {
-    if (length(unique(x$bin_width)) > 1 && sheppard) {
+    if (length(unique(bin_width)) > 1 && sheppard) {
       stop('Non-equal bin widths with Sheppard\'s correction.')
     }
-    adj <- (- x$bin_width[1]^2 / 12)
+    adj <- (- bin_width[1]^2 / 12)
   }
 
   return(weighted.var(x = midpoints, w = x$histogram_data) + adj)
 }
 
-#' weighted.sd.Histogram computes a the sample variance of a Histogram or GenomicHistogram, with option for Sheppard's correction.
+#' Sample sd of a Histogram or GenomicHistogram
 #'
 #' @param x Histogram or GenomicHistogram
+#'
 #'
 #' @rdname weighted.moments
 #' @exportS3Method weighted.sd Histogram
@@ -56,32 +56,36 @@ weighted.sd.Histogram <- function(x, sheppard = FALSE) {
   return(sqrt(weighted.var.Histogram(x, sheppard)))
 }
 
-#' Computes a the sample mean of a Histogram or GenomicHistogram
+#' Sample skewness of a Histogram or GenomicHistogram
 #'
 #' @param x Histogram or GenomicHistogram
 #'
 #' @rdname weighted.moments
 #' @exportS3Method weighted.skewness Histogram
+#' @examples
+#' x <- Histogram(c(1,1,2,2,3,3,4,3,2,1))
+#' weighted.mean(x)
+#' weighted.sd(x)
+#' weighted.var(x)
+#' weighted.skewness(x)
 weighted.skewness.Histogram <- function(x, ...) {
   midpoints <- (x$interval_end + x$interval_start) / 2
   weighted.skewness(x = midpoints, w = x$histogram_data, ...)
 }
 
-#' Computed the weighted sample standard deviation
-#'
 #' @param x values whose weighted sd is to be computed
 #' @param w numerical vector of weight the same length as x
 #' @export
+#' @rdname weighted.moments
 weighted.sd.default <- function(x, w) {
   stopifnot(length(x) == length(w))
   return(sqrt(weighted.var(x, w)))
 }
 
-#' Computes the weighted sample skewness
-#'
 #' @param x values whose weighted variance is to be computed
 #' @param w numerical vector of weight the same length as x
 #' @export
+#' @rdname weighted.moments
 weighted.var.default <- function(x, w, biased = FALSE) {
   stopifnot(length(x) == length(w))
   mu <- weighted.mean(x, w)
@@ -90,17 +94,21 @@ weighted.var.default <- function(x, w, biased = FALSE) {
   return((sum(w * x^2) - N * mu^2) / denom)
 }
 
-#' Computes the weighted sample skewness
-#'
-#' Either compute 'b' or 'g' skewness using the notation from: Joanes, D. N., & Gill, C. A. (1998). Comparing Measures of Sample Skewness and Kurtosis. Journal of the Royal Statistical Society.
-#' 'b' skewness follows MINITAB and uses the unbiased formula for sd with (N - 1) in the denominator
-#' 'g' skewness follows SAS format (and that of the R moments::skewness) and uses the biased formula for sd with N in the denominator
+#' @section Sample Skewness:
+#' Sample skewness either computes 'b' or 'g' skewness using the notation from [1].
+#' \itemize{
+#'   \item `b` skewness follows MINITAB and uses the unbiased formula for sd with (N - 1) in the denominator
+#'   \item `g` skewness follows SAS format (and that of the R moments::\link[moments]{skewness}) and uses the biased formula for sd with N in the denominator
+#' }
 #' "For small samples from a normal distribution b has smaller mean-squared error than g... for small samples from non-normal distributions... g has a smaller mean-squared error."
+#'
+#' @references
+#' [1] Joanes, D. N., & Gill, C. A. (1998). Comparing Measures of Sample Skewness and Kurtosis. Journal of the Royal Statistical Society. Series D (The Statistician), 47(1), 183–189. http://www.jstor.org/stable/2988433
 #'
 #' @param x values whose weighted skewness is to be computed
 #' @param w numerical vector of weight the same length as x
-#' @param type 'b' for (N - 1) adjustment to variance and 'g' for method of moments estimator
-#'
+#' @param type 'b' for (N - 1) adjustment to variance and 'g' for method of moments estimator.
+#' @rdname weighted.moments
 #' @export
 weighted.skewness.default <- function(x, w, type = c('b', 'g')) {
   stopifnot(length(x) == length(w))
