735340427fe7c983c1e2a52063d7e23a9d6920d8
helen-zhu <helen.m.zhu@gmail.com>
2023-04-24 23:54:24 -0400

    docs: adding rdname to s3 methods

--- R/find_change_points.R
+++ R/find_change_points.R
@@ -1,17 +1,21 @@
 #' Find changepoints in a vector with uniform stretches of values
 #' @param x A numeric vector or Histogram object
-#' 
+#'
 #' @return A numeric vector representing change point indices
+#'
+#' @rdname find_change_points
 #' @export
 find_change_points <- function(x){
   UseMethod("find_change_points")
 }
 
+#' @rdname find_change_points
 #' @exportS3Method find_change_points Histogram
 find_change_points.Histogram <- function(x){
   find_change_points.numeric(x$histogram_data)
 }
 
+#' @rdname find_change_points
 #' @exportS3Method find_change_points numeric
 find_change_points.numeric <- function(x){
   change_points <- which(diff(x) != 0)
