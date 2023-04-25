2d4059ca07c92d10a1f087414e702516f4007208
helen-zhu <helen.m.zhu@gmail.com>
2023-04-25 00:36:09 -0400

    docs: updating docs for the gamma distribution series

--- R/tgamma.R
+++ R/tgamma.R
@@ -1,63 +1,48 @@
 
-#' truncated gamma
+#' The Truncated Gamma Distribution
+#' 
+#' Density, distribution function, quantile function and random generation for the Truncated Gamma distribution with endpoints
+#' at [a, b]. Support provided by the `truncdist` package with all parameters available for a standard Gamma distribution. 
+#' One additional parameter, `shift` is provided to generate Gamma distributions with non-zero starts, starting at the `shift` value.
 #'
 #' @param x numeric vector of quantiles
-#' @param shape shape parameter of gamma distribution
-#' @param rate rate parameter of gamma distribution, default 1
-#' @param shift shift of the left end of the distribution
-#' @param a distribution start
-#' @param b distribution end
-#' @param ... additional parameters to be passed to truncdist::dtrunc
-#'
-#' @return density of distribution
-#' @export
-dtgamma <- function(x, shape, rate = 1, shift = 0, a = 0, b = Inf, ...) {
-  truncdist::dtrunc(x - shift, spec = 'gamma', a = a, b = b, shape = shape, rate = rate, ...)
-}
-
-#' truncated gamma
-#'
 #' @param q numeric vector of quantiles
-#' @param shape shape parameter of gamma distribution
-#' @param rate rate parameter of gamma distribution, default 1
-#' @param shift shift of the left end of the distribution
-#' @param a distribution start
-#' @param b distribution end
-#' @param ... additional parameters to be passed to truncdist::dtrunc
-#'
-#' @return distribution function
-#' @export
-ptgamma <- function(q, shape, rate = 1, shift = 0, a = 0, b = Inf, ...) {
-  truncdist::ptrunc(q - shift, spec = 'gamma', a = a, b = b, shape = shape, rate = rate, ...)
-}
-
-#' truncated gamma
-#'
 #' @param n number of observations
-#' @param shape shape parameter of gamma distribution
-#' @param rate rate parameter of gamma distribution, default 1
-#' @param shift shift of the left end of the distribution
-#' @param a distribution start
-#' @param b distribution end
-#' @param ... additional parameters to be passed to truncdist::dtrunc
-#'
-#' @return random deviates
-#' @export
-rtgamma <- function(n, shape, rate = 1, shift = 0, a = 0, b = Inf, ...) {
-  shift + truncdist::rtrunc(n, spec = 'gamma', a = a, b = b, shape = shape, rate = rate, ...)
-}
-
-#' truncated gamma
-#'
 #' @param p vector of probabilities
 #' @param shape shape parameter of gamma distribution
 #' @param rate rate parameter of gamma distribution, default 1
 #' @param shift shift of the left end of the distribution
 #' @param a distribution start
 #' @param b distribution end
-#' @param ... additional parameters to be passed to truncdist::dtrunc
+#' @param ... additional parameters to be passed to truncdist::dtrunc, truncdist::ptrunc, truncdist::rtrunc and truncdist::qtrunc
 #'
+#' @return density of distribution
+#' 
+#' @rdname tgamma
+#' @export
+dtgamma <- function(x, shape, rate = 1, shift = 0, a = 0, b = Inf, ...) {
+  truncdist::dtrunc(x - shift, spec = 'gamma', a = a, b = b, shape = shape, rate = rate, ...)
+}
+
+#' @return distribution function
+#'
+#' @rdname tgamma
+#' @export
+ptgamma <- function(q, shape, rate = 1, shift = 0, a = 0, b = Inf, ...) {
+  truncdist::ptrunc(q - shift, spec = 'gamma', a = a, b = b, shape = shape, rate = rate, ...)
+}
+
+#' @return random deviates
+#'
+#' @rdname tgamma
+#' @export
+rtgamma <- function(n, shape, rate = 1, shift = 0, a = 0, b = Inf, ...) {
+  shift + truncdist::rtrunc(n, spec = 'gamma', a = a, b = b, shape = shape, rate = rate, ...)
+}
+
 #' @return quantile function
+#'
+#' @rdname tgamma
 #' @export
 qtgamma <- function(p, shape, rate = 1, shift = 0, a = 0, b = Inf, ...) {
   shift + truncdist::qtrunc(p, spec = 'gamma', a = a, b = b, shape = shape, rate = rate, ...)
--- R/tgamma_flip.R
+++ R/tgamma_flip.R
@@ -1,55 +1,14 @@
 
-#' truncated gamma flip
+#' The Truncated Flipped Gamma Distribution
 #'
+#' Density, distribution function, quantile function and random generation for the Truncated Flipped Gamma distribution,
+#' a Gamma distribution reflected across the Y-axis, and truncted at endpoints [a, b]. Support provided by the `truncdist` package
+#' with all parameters available for a standard Gamma distribution. One additional parameter, `offset` is provided to 
+#' generate Truncated Flipped Gamma distributions with non-zero right endpoints, ending at the `offset` value.
+#' 
 #' @param x numeric vector of quantiles
-#' @param shape shape parameter of gamma distribution
-#' @param rate rate parameter of gamma distribution, default 1
-#' @param offset offset of the right end of the distribution
-#' @param a distribution start
-#' @param b distribution end
-#' @param ... additional parameters to be passed to truncdist::dtrunc
-#'
-#' @return density of distribution
-#' @export
-dtgamma_flip <- function(x, shape, rate = 1, offset = 0, a = 0, b = Inf, ...) {
-  truncdist::dtrunc(offset - x, spec = 'gamma', a = a, b = b, shape = shape, rate = rate, ...)
-}
-
-#' truncated gamma flip
-#'
 #' @param q numeric vector of quantiles
-#' @param shape shape parameter of gamma distribution
-#' @param rate rate parameter of gamma distribution, default 1
-#' @param offset offset of the right end of the distribution
-#' @param a distribution start
-#' @param b distribution end
-#' @param lower.tail logical; if TRUE (default), probabilities are P[X <= x], otherwise, P[X > x]
-#' @param ... additional parameters to be passed to truncdist::dtrunc
-#'
-#' @return distribution function
-#' @export
-ptgamma_flip <- function(q, shape, rate = 1, offset = 0, a = 0, b = Inf, lower.tail = TRUE, ...) {
-  truncdist::ptrunc(offset - q, spec = 'gamma', a = a, b = b, shape = shape, rate = rate, lower.tail = !lower.tail, ...)
-}
-
-#' truncated gamma flip
-#'
 #' @param n number of observations
-#' @param shape shape parameter of gamma distribution
-#' @param rate rate parameter of gamma distribution, default 1
-#' @param offset offset of the right end of the distribution
-#' @param a distribution start
-#' @param b distribution end
-#' @param ... additional parameters to be passed to truncdist::dtrunc
-#'
-#' @return random deviates
-#' @export
-rtgamma_flip <- function(n, shape, rate = 1, offset = 0, a = 0, b = Inf, ...) {
-  offset - truncdist::rtrunc(n, spec = 'gamma', a = a, b = b, shape = shape, rate = rate, ...)
-}
-
-#' truncated gamma
-#'
 #' @param p vector of probabilities
 #' @param shape shape parameter of gamma distribution
 #' @param rate rate parameter of gamma distribution, default 1
@@ -57,9 +16,35 @@ rtgamma_flip <- function(n, shape, rate = 1, offset = 0, a = 0, b = Inf, ...) {
 #' @param a distribution start
 #' @param b distribution end
 #' @param lower.tail logical; if TRUE (default), probabilities are P[X <= x], otherwise, P[X > x]
-#' @param ... additional parameters to be passed to truncdist::dtrunc
-#'
+#' @param ... additional parameters to be passed to truncdist::dtrunc, truncdist::ptrunc, truncdist::rtrunc and truncdist::qtrunc
+#' 
+#' @return density of distribution
+#' 
+#' @rdname tgamma_flip
+#' @export
+dtgamma_flip <- function(x, shape, rate = 1, offset = 0, a = 0, b = Inf, ...) {
+  truncdist::dtrunc(offset - x, spec = 'gamma', a = a, b = b, shape = shape, rate = rate, ...)
+}
+
+#' @return distribution function
+#' 
+#' @rdname tgamma_flip
+#' @export
+ptgamma_flip <- function(q, shape, rate = 1, offset = 0, a = 0, b = Inf, lower.tail = TRUE, ...) {
+  truncdist::ptrunc(offset - q, spec = 'gamma', a = a, b = b, shape = shape, rate = rate, lower.tail = !lower.tail, ...)
+}
+
+#' @return random deviates
+#' 
+#' @rdname tgamma_flip
+#' @export
+rtgamma_flip <- function(n, shape, rate = 1, offset = 0, a = 0, b = Inf, ...) {
+  offset - truncdist::rtrunc(n, spec = 'gamma', a = a, b = b, shape = shape, rate = rate, ...)
+}
+
 #' @return quantile function
+#' 
+#' @rdname tgamma_flip
 #' @export
 qtgamma_flip <- function(p, shape, rate = 1, offset = 0, a = 0, b = Inf, lower.tail = TRUE, ...) {
   offset - truncdist::qtrunc(p, spec = 'gamma', a = a, b = b, shape = shape, rate = rate, lower.tail = !lower.tail, ...)
