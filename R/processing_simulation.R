0789e71ab1964407f82b35ec692090034393f03f
Stefan Eng <stefaneng@mednet.ucla.edu>
2023-07-17 15:14:42 -0700

    Add temporary compute of peak_min and peak_max merge_sim

--- R/processing_simulation.R
+++ R/processing_simulation.R
@@ -1,3 +1,50 @@
+# This is just temporary to add peak_min/peak_max to missing sims
+peak_min_recompute <- function(
+    N = c(25, 500),
+    unif_length = c(6, 25),
+    norm_sd = c(1, 4),
+    gamma_shape = c(1, 4),
+    eps = c(0.05, .5),
+    noise = c(.05, .5),
+    max_uniform = c(TRUE, FALSE),
+    remove_low_entropy = c(TRUE, FALSE),
+    truncated_models = c(TRUE, FALSE),
+    actual_dist = c('norm', 'unif', 'gamma'),
+    metrics = c('mle', 'jaccard', 'intersection', 'ks', 'mse', 'chisq'),
+    seed = as.integer(Sys.time()),
+    ...
+  ) {
+
+  .sample_unif <- function(x, n = 1) {
+    runif(n, min = x[1], max = x[2])
+  }
+  set.seed(seed);
+
+  N_sim <- .sample_unif(N)
+  eps_sample <- .sample_unif(eps)
+  noise_sim <- .sample_unif(noise)
+  N_noise_sim <- round(N_sim * noise_sim)
+
+  if (actual_dist == 'norm') {
+    param <- .sample_unif(norm_sd)
+    peak <- rnorm(N_sim, mean = 0, sd = param)
+  } else if (actual_dist == 'gamma') {
+    param <- .sample_unif(gamma_shape)
+    peak <- rgamma(N_sim, shape = param)
+  } else {
+    # Unif
+    param <- .sample_unif(unif_length)
+    peak <- runif(N_sim, min = 0, max = param)
+  }
+
+  data_sd <- sd(peak)
+  peak_min <- min(peak)
+  peak_max <- max(peak)
+  noise_min <- peak_min - data_sd
+  noise_max <- peak_max + data_sd
+  list(seed = seed, peak_min = peak_min, peak_max = peak_max, noise_min = noise_min)
+}
+
 process_sim <- function(x) {
     res <- x[c('N', 'dist', 'param', 'noise', 'eps')]
     names(res) <- c('N', 'actual_dist', 'param', 'noise', 'eps')
@@ -29,4 +76,9 @@ summarize_results_error <- function(x) {
 
 overlap_size <- function(a1, a2, b1, b2) {
   max(0, min(a2, b2) - max(a1, b1))
+}
+
+union_size <- function(a1, a2, b1, b2) {
+  if (overlap_size(a1, a2, b1, b2) > 0) max(a2, b2) - min(a1, b1)
+  else (a2 - a1) + (b2 - b1)
 }
--- inst/merge_sim_data.R
+++ inst/merge_sim_data.R
@@ -7,19 +7,16 @@ base.path <- HistogramZoo:::load.config()$root.path;
 results.folder <- file.path(base.path, 'results');
 merged.folder <- file.path(results.folder, 'merged_sims');
 
-sim.folders <- c('unimodal_sim_noise_v2');
+# v1-v3 need to have peak_min, peak_max computed
+sim.folders <- c('unimodal_sim_noise', 'unimodal_sim_noise_v2', 'unimodal_sim_noise_v3');
 
 for (sim in sim.folders) {
-    cat('Processing: ', sim, ' files...\n');
+  for (mle in c(FALSE, TRUE)) {
+    cat('Processing: ', sim, ' files... MLE = ', mle, '\n');
     sim.folder <- file.path(results.folder, sim);
-    sim.tsv.mle.paths <- list.files(sim.folder, pattern = 'Unimodal_Sim_MLE.tsv$', full.names = TRUE);
-    # sim.tsv.mle.paths <- sim.tsv.mle.paths[c(1, length(sim.tsv.mle.paths))];
-    sim.tsv.paths <- list.files(sim.folder, pattern = 'Unimodal_Sim.tsv$', full.names = TRUE);
+    mle_pattern <- if (mle) 'Unimodal_Sim_MLE.tsv$' else 'Unimodal_Sim.tsv$'
 
-    sim.mle.data <- rbindlist(
-        lapply(sim.tsv.mle.paths, data.table::fread, sep = '\t'),
-        fill = TRUE
-        )
+    sim.tsv.paths <- list.files(sim.folder, pattern = , full.names = TRUE);
 
     sim.data <- rbindlist(
         lapply(sim.tsv.paths, data.table::fread, sep = '\t'),
@@ -28,33 +25,47 @@ for (sim in sim.folders) {
 
     sim.data$seg_length <-  sim.data$end - sim.data$start;
 
+    if (! c('peak_min') %in% colnames(sim.data)) {
+      unique.recompute <- unique(
+      sim.data[, c('seed', 'actual_dist', 'max_uniform', 'remove_low_entropy', 'truncated_models')]
+      )
+
+    recompute.peak.rage <- do.call(
+      'rbind.data.frame',
+      mapply(
+        HistogramZoo:::peak_min_recompute,
+        seed = unique.recompute$seed,
+        actual_dist = unique.recompute$actual_dist,
+        max_uniform = unique.recompute$max_uniform,
+        remove_low_entropy = unique.recompute$remove_low_entropy,
+        SIMPLIFY = FALSE
+        )
+      )
+
+    sim.data <- merge(
+      x = sim.data,
+      y = recompute.peak.rage,
+      by = 'seed',
+      suffixes = c('', '.validation')
+      )
+    }
+
+    mle.suffix <- if (mle) '-mle' else '';
+
     write.table(
         sim.data,
         file = print(file.path(
             merged.folder,
             generate.filename(
                 'HZSimulation',
-                'merged-unimodal-sim-noise',
+                paste0('merged-unimodal-sim-noise', mle.suffix),
                 'tsv'
                 )
             )),
         sep = '\t',
         row.names = FALSE
         )
-
-    write.table(
-        sim.mle.data,
-        file = print(file.path(
-            merged.folder,
-            generate.filename(
-                'HZSimulation',
-                'merged-unimodal-sim-noise-MLE',
-                'tsv'
-                )
-            )),
-        sep = '\t',
-        row.names = FALSE
-        )
+    }
 }
 
 
