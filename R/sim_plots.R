be1c5b371748b4809d852252e7040f14922a17dd
Stefan Eng <stefaneng@mednet.ucla.edu>
2023-07-17 10:49:41 -0700

    Continuous color legends

--- R/sim_plots.R
+++ R/sim_plots.R
@@ -1,12 +1,28 @@
-max.uniform.col <- 'firebrick3';
-remove.low.entropy.col <- 'dodgerblue2';
+covariate.col <- list(
+  max.uniform = 'firebrick3',
+  remove.low.entropy = 'dodgerblue2',
+  N.decile = 'slateblue4',
+  noise.decile = 'darkorange1',
+  eps.decile = 'seagreen3',
+  param.decile = 'yellowgreen'
+  )
+
 
 common.sim.legend <- function(
     include.legends = c('params', 'distributions', 'quantiles'),
-    params.to.include = c('max_uniform', 'remove_low_entropy')
+    params.to.include = c(
+      'max_uniform', 'remove_low_entropy'
+      ),
+    cont.params.to.include = c(
+      'N_decile', 'noise_decile',
+      'eps_decile', 'param_decile'
+    )
   ) {
   include.legends <- match.arg(include.legends, several.ok = TRUE);
   params.to.include <- match.arg(params.to.include, several.ok = TRUE);
+  if (! is.null(cont.params.to.include)) {
+    cont.params.to.include <- match.arg(cont.params.to.include, several.ok = TRUE);
+    }
 
   legend.list <- list()
   if ('params' %in% include.legends) {
@@ -16,7 +32,7 @@ common.sim.legend <- function(
       legend.list,
       list(
         legend = list(
-            colours = c(max.uniform.col, remove.low.entropy.col)[params.select],
+            colours = c(covariate.col$max.uniform, covariate.col$remove.low.entropy)[params.select],
             title = "Parameters",
             labels = c('Max uniform', 'Remove low entropy')[params.select],
             size = 3,
@@ -44,16 +60,32 @@ common.sim.legend <- function(
       )
     }
 
-  common.params <- list(
+  if (! is.null(cont.params.to.include)) {
+    cont.params <- gsub('_', '.', cont.params.to.include)
 
-    )
+    cont.params.legend <- lapply(cont.params, function(p) {
+      list(
+        colours = c('white', covariate.col[[p]]),
+        labels = c('0', '1'), # TODO
+        title = sub('[.]decile', '', p),
+        angle = -90,
+        width = 6,
+        continuous = TRUE
+        )
+      })
+
+    names(cont.params.legend) <- rep('legend', length(cont.params.legend))
+
+    legend.list <- c(legend.list, cont.params.legend)
+    }
 
   BoutrosLab.plotting.general::legend.grob(
     legends = legend.list,
     label.cex = 1.5,
     title.cex = 1.5,
     size = 3,
-    layout = c(1,length(include.legends))
+    title.just = 'left',
+    layout = c(1,length(legend.list))
     )
   }
 
@@ -73,25 +105,25 @@ assign.cov.factor.col <- function(x, ramp.colors) {
 
 sim.plot.heatmap.cov <- function(cov.data) {
   if ('max_uniform' %in% colnames(cov.data)) {
-    cov.data$max_uniform <- ifelse(cov.data$max_uniform, 'firebrick3', 'white');
+    cov.data$max_uniform <- ifelse(cov.data$max_uniform, covariate.col$max.uniform, 'white');
   }
   if ('remove_low_entropy' %in% colnames(cov.data)) {
-    cov.data$remove_low_entropy <- ifelse(cov.data$remove_low_entropy, 'dodgerblue', 'white');
+    cov.data$remove_low_entropy <- ifelse(cov.data$remove_low_entropy, covariate.col$remove.low.entropy, 'white');
   }
   if ('actual_dist' %in% colnames(cov.data)) {
     cov.data$actual_dist <- unname(distribution_colours[cov.data$actual_dist]);
   }
   if ('N_decile' %in% colnames(cov.data)) {
-    cov.data$N_decile <- assign.cov.factor.col(cov.data$N_decile, c('white', 'slateblue4'));
+    cov.data$N_decile <- assign.cov.factor.col(cov.data$N_decile, c('white', covariate.col$N.decile));
   }
   if ('noise_decile' %in% colnames(cov.data)) {
-    cov.data$noise_decile <- assign.cov.factor.col(cov.data$noise_decile, c('white', 'darkorange1'));
+    cov.data$noise_decile <- assign.cov.factor.col(cov.data$noise_decile, c('white', covariate.col$noise.decile));
   }
   if ('eps_decile' %in% colnames(cov.data)) {
-    cov.data$eps_decile <- assign.cov.factor.col(cov.data$eps_decile, c('white', 'seagreen3'));
+    cov.data$eps_decile <- assign.cov.factor.col(cov.data$eps_decile, c('white', covariate.col$eps.decile));
   }
   if ('param_decile' %in% colnames(cov.data)) {
-    cov.data$param_decile <- assign.cov.factor.col(cov.data$param_decile, c('white', 'yellowgreen'));
+    cov.data$param_decile <- assign.cov.factor.col(cov.data$param_decile, c('white', covariate.col$param.decile));
   }
 
   if (ncol(cov.data) == 1) cov.data <- cbind(cov.data[, 1], cov.data[, 1]);
@@ -109,7 +141,10 @@ sim.plot.heatmap.cov <- function(cov.data) {
 
 #' @export
 #' @import data.table
-sim.plot.overall.accuracy <- function(x, acc = c('dist', 'peaks', 'both'), ...) {
+sim.plot.overall.accuracy <- function(
+    x,
+    acc = c('dist', 'peaks', 'both'),
+    ...) {
   acc <- match.arg(acc);
   # Accuracy by distribution
   overall.accuracy <- x[
@@ -158,7 +193,9 @@ sim.plot.overall.accuracy <- function(x, acc = c('dist', 'peaks', 'both'), ...) 
     x.spacing = c(-0.25, 0),
     width = 12,
     legend = list(right = list(
-      fun = common.sim.legend()
+      fun = common.sim.legend(
+          cont.params.to.include = NULL
+          )
         )
       ),
     layout.width = 2,
@@ -168,7 +205,10 @@ sim.plot.overall.accuracy <- function(x, acc = c('dist', 'peaks', 'both'), ...) 
     )
 }
 
-sim.plot.quantile.accuracy <- function(x, acc = c('dist', 'peaks', 'both'), sort.cols = NULL, ...) {
+sim.plot.quantile.accuracy <- function(
+    x,
+    acc = c('dist', 'peaks', 'both'),
+    sort.cols = NULL, ...) {
   acc <- match.arg(acc);
   decile.accuracy <- x[
     , .(
@@ -186,6 +226,8 @@ sim.plot.quantile.accuracy <- function(x, acc = c('dist', 'peaks', 'both'), sort
     actual_dist + max_uniform + remove_low_entropy + N_decile + noise_decile + eps_decile ~ metric,
     value.var = paste0('accuracy_', acc)
     )
+
+  cont.cols <- colnames(decile.wide.accuracy.dist)[grepl('_decile', colnames(decile.wide.accuracy.dist))];
 
   if (!is.null(sort.cols)) {
     setorderv(decile.wide.accuracy.dist, sort.cols);
@@ -221,9 +263,12 @@ sim.plot.quantile.accuracy <- function(x, acc = c('dist', 'peaks', 'both'), sort
     x.spacing = c(-0.25, 0),
     width = 12,
     legend = list(right = list(
-      fun = common.sim.legend(include.legends = c('distributions', 'quantiles'))
+      fun = common.sim.legend(
+        include.legends = c('distributions', 'quantiles'),
+        cont.params.to.include = cont.cols
         )
-      ),
+      )
+    ),
     layout.width = 2,
     layout.height = 1,
     xlab.label = xlab.label,
